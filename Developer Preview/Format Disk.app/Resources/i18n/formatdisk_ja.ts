df11f5e4836407141d4e84bc0a8e68eff3ddd59c
Weblate <noreply@weblate.org>
2021-10-06 15:03:47 +0200

    Added translation using Weblate (Japanese)

---
+++ Developer Preview/Format Disk.app/Resources/i18n/formatdisk_ja.ts
@@ -0,0 +1,83 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="ja" sourcelanguage="en">
+<context>
+    <name>@default</name>
+    <message>
+        <location filename="../formatdisk.py" line="53"/>
+        <source>Erase</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../formatdisk.py" line="54"/>
+        <source>Cancel</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>Window</name>
+    <message>
+        <location filename="../formatdisk.ui" line="29"/>
+        <source>Format Disk</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../formatdisk.ui" line="99"/>
+        <source>Erase &quot;%1&quot; (%2)?</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../formatdisk.ui" line="112"/>
+        <source>Erasing &quot;%1&quot; will permanently delete all data stored on it, and cannot be undone.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../formatdisk.ui" line="134"/>
+        <source>Name:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../formatdisk.ui" line="141"/>
+        <source>Untitled</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../formatdisk.ui" line="148"/>
+        <source>Format:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../formatdisk.ui" line="156"/>
+        <source>Placeholder</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../formatdisk.ui" line="164"/>
+        <source>Scheme:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../formatdisk.ui" line="172"/>
+        <source>GUID Partition Map</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../formatdisk.ui" line="177"/>
+        <source>Master Boot Record</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../formatdisk.ui" line="185"/>
+        <source>Overwrite with zeros</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>tstr</name>
+    <message>
+        <location filename="../tstranslator.py" line="76"/>
+        <source>Hello World</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
