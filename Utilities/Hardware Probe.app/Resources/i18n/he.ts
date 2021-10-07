618286cbec6f91cfe117501cca7fdc098c148efd
Yaron Shahrabani <sh.yaron@gmail.com>
2021-10-07 21:28:57 +0000

    Translated using Weblate (Hebrew)
    Currently translated at 100.0% (20 of 20 strings)
    
    Translation: helloSystem/Hardware Probe
    Translate-URL: https://hosted.weblate.org/projects/hellosystem/hardware-probe/he/

--- Utilities/Hardware Probe.app/Resources/i18n/he.ts
+++ Utilities/Hardware Probe.app/Resources/i18n/he.ts
@@ -5,88 +5,90 @@
     <name>@default</name>
     <message>
         <source>An unknown error occured.</source>
-        <translation type="unfinished"></translation>
+        <translation>אירעה שגיאה בלתי ידועה.</translation>
     </message>
     <message>
         <source>Hardware Probe</source>
-        <translation type="unfinished"></translation>
+        <translation>תשאול חומרה</translation>
     </message>
     <message>
         <source>Privacy Information</source>
-        <translation type="unfinished"></translation>
+        <translation>מידע על פרטיות</translation>
     </message>
     <message>
         <source>Uploading a Hardware Probe is subject to the following Pricacy Terms.</source>
-        <translation type="unfinished"></translation>
+        <translation>העלאת תשאול החומרה כפוף לתנאי הפרטיות הבאים.</translation>
     </message>
     <message>
         <source>Please see %s for more information.</source>
-        <translation type="unfinished"></translation>
+        <translation>נא לקרוא את %s לפרטים נוספים.</translation>
     </message>
     <message>
         <source>&lt;p&gt;This utility collects hardware details of your computer and can anonymously upload them to a public database.&lt;/p&gt;
         &lt;p&gt;This can help users and operating system developers to collaboratively debug hardware related issues, check for operating system compatibility and find drivers.&lt;/p&gt;
         &lt;p&gt;You will get a permanent probe URL to view and share collected information.&lt;/p&gt;&lt;br&gt;&lt;br&gt;&lt;br&gt;</source>
-        <translation type="unfinished"></translation>
+        <translation>&lt;p&gt;כלי זה אוסף פרטים על החומרה של המחשב שלך ויכול לשלוח אותם באופן אלמוני למסד נתונים ציבורי.&lt;/p&gt;
+        &lt;p&gt;הוא יכול לסייע למשתמשים ולמפתחי מערכות הפעלה לפתור תקלות מעולם החומרה באופן שיתופי, לבדוק תאימות ולמצוא מנהלי התקנים.&lt;/p&gt;
+        &lt;p&gt;תוקצה לך כתובת תשאול קבועה כדי לצפות ולשתף את המידע שנאסף.&lt;/p&gt;&lt;br&gt;&lt;br&gt;&lt;br&gt;</translation>
     </message>
     <message>
         <source>Show Hardware Probe</source>
-        <translation type="unfinished"></translation>
+        <translation>הצגת תשאול חומרה</translation>
     </message>
     <message>
         <source>Failed to open the hardware probe.</source>
-        <translation type="unfinished"></translation>
+        <translation>פתיחת תשאול החומרה נכשלה.</translation>
     </message>
     <message>
         <source>Failed to run the %s tool.</source>
-        <translation type="unfinished"></translation>
+        <translation>הפעלת הכלי %s נכשלה.</translation>
     </message>
     <message>
         <source>Failed to set the owner to %x.</source>
-        <translation type="unfinished"></translation>
+        <translation>הגדרת הבעלות אל %x.</translation>
     </message>
     <message>
         <source>Uploading Hardware Probe</source>
-        <translation type="unfinished"></translation>
+        <translation>תשאל החומרה נשלח</translation>
     </message>
     <message>
         <source>The Hardware Probe is being uploaded to the public database</source>
-        <translation type="unfinished"></translation>
+        <translation>תשאול החומרה נשלח למסד הנתונים הציבורי</translation>
     </message>
     <message>
         <source>You need an active internet connection in order to upload.</source>
-        <translation type="unfinished"></translation>
+        <translation>נדרש חיבור פעיל לאינטרנט כדי לשלוח.</translation>
     </message>
     <message>
         <source>Failed to upload using the %s tool.</source>
-        <translation type="unfinished"></translation>
+        <translation>ההעלאה באמצעות הכלי %s נכשלה.</translation>
     </message>
     <message>
         <source>Hardware Probe Uploaded</source>
-        <translation type="unfinished"></translation>
+        <translation>תשאול החומרה נשלח</translation>
     </message>
     <message>
         <source>Quit</source>
-        <translation type="unfinished"></translation>
+        <translation>יציאה</translation>
     </message>
     <message>
         <source>Failed to open the uploaded hardware probe.</source>
-        <translation type="unfinished"></translation>
+        <translation>פתיחת תשאול החומרה שנשלח נכשלה.</translation>
     </message>
     <message>
         <source>Error</source>
-        <translation type="unfinished"></translation>
+        <translation>שגיאה</translation>
     </message>
     <message>
         <source>Hardware Probe was not successful.</source>
-        <translation type="unfinished"></translation>
+        <translation>תשאול החומרה לא הצליח.</translation>
     </message>
 </context>
 <context>
     <name>tstr</name>
     <message>
         <source>Hello World</source>
-        <translation type="unfinished"></translation>
+        <translation>שלום עולם</translation>
     </message>
 </context>
 </TS>
