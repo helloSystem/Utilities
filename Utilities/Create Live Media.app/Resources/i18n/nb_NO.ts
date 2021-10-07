450407081f42af7196c65f006faadb961562e905
probonopd <probono@puredarwin.org>
2021-10-07 18:59:16 +0200

    Added translation using Weblate (Norwegian Bokmål)

---
+++ Utilities/Create Live Media.app/Resources/i18n/nb_NO.ts
@@ -0,0 +1,126 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="nb_NO">
+<context>
+    <name>@default</name>
+    <message>
+        <source>Create Live Media</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>This will download a Live image and will write it to an attached storage device.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Other...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Local ISO file...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Please choose an image:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Date</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Show Pre-release builds</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Other</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>URL of the ISO</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Disk images (*.iso *.img);;All files (*.*)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>This requires an active internet connection.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Select Destination Disk</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>All data on the selected disk will be erased.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Could not determine the required disk space.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Disk space required: %s MiB</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Warning</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>This will erase all contents of this disk and install the live system on it. Continue?</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Downloading and writing Live medium</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>The Live image is being downloaded and written to the medium.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Could not unmount parititons.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>An error occured while trying to write the image. Is the download URL accessible? Were all partitions unmounted? Do you have write permissions there?</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Live Medium Complete</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>The Live image has been written to the device.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>You can now start your computer from the Live medium.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Quit</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Device was unplugged, exiting</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Error</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>The installation could not be performed.</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>tstr</name>
+    <message>
+        <source>Hello World</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
