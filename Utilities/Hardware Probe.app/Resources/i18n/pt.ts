38d8bf80a0f4ef983edd0cb17ccd0989f57d0150
probonopd <probono@puredarwin.org>
2021-10-07 21:35:37 +0200

    Added translation using Weblate (Portuguese)

---
+++ Utilities/Hardware Probe.app/Resources/i18n/pt.ts
@@ -0,0 +1,92 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="pt">
+<context>
+    <name>@default</name>
+    <message>
+        <source>An unknown error occured.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Hardware Probe</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Privacy Information</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Uploading a Hardware Probe is subject to the following Pricacy Terms.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Please see %s for more information.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>&lt;p&gt;This utility collects hardware details of your computer and can anonymously upload them to a public database.&lt;/p&gt;
+        &lt;p&gt;This can help users and operating system developers to collaboratively debug hardware related issues, check for operating system compatibility and find drivers.&lt;/p&gt;
+        &lt;p&gt;You will get a permanent probe URL to view and share collected information.&lt;/p&gt;&lt;br&gt;&lt;br&gt;&lt;br&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Show Hardware Probe</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Failed to open the hardware probe.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Failed to run the %s tool.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Failed to set the owner to %x.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Uploading Hardware Probe</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>The Hardware Probe is being uploaded to the public database</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>You need an active internet connection in order to upload.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Failed to upload using the %s tool.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Hardware Probe Uploaded</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Quit</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Failed to open the uploaded hardware probe.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Error</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Hardware Probe was not successful.</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>tstr</name>
+    <message>
+        <source>Hello World</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
