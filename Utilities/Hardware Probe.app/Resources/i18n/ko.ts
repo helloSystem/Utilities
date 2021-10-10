5a896f373fe11a7667299079882577632b637057
louies0623 <louies0623@gmail.com>
2021-10-10 08:13:04 +0000

    Translated using Weblate (Korean)
    Currently translated at 100.0% (20 of 20 strings)
    
    Translation: helloSystem/Hardware Probe
    Translate-URL: https://hosted.weblate.org/projects/hellosystem/hardware-probe/ko/

--- Utilities/Hardware Probe.app/Resources/i18n/ko.ts
+++ Utilities/Hardware Probe.app/Resources/i18n/ko.ts
@@ -5,88 +5,90 @@
     <name>@default</name>
     <message>
         <source>An unknown error occured.</source>
-        <translation type="unfinished"></translation>
+        <translation>알 수없는 오류가 발생했습니다.</translation>
     </message>
     <message>
         <source>Hardware Probe</source>
-        <translation type="unfinished"></translation>
+        <translation>하드웨어 감지</translation>
     </message>
     <message>
         <source>Privacy Information</source>
-        <translation type="unfinished"></translation>
+        <translation>콘텐츠 개인 정보 보호</translation>
     </message>
     <message>
         <source>Uploading a Hardware Probe is subject to the following Pricacy Terms.</source>
-        <translation type="unfinished"></translation>
+        <translation>업로드 하드웨어 감지에는 다음 개인정보 보호 동의가 적용됩니다.</translation>
     </message>
     <message>
         <source>Please see %s for more information.</source>
-        <translation type="unfinished"></translation>
+        <translation>자세한 내용은 %s을(를) 참조하십시오.</translation>
     </message>
     <message>
         <source>&lt;p&gt;This utility collects hardware details of your computer and can anonymously upload them to a public database.&lt;/p&gt;
         &lt;p&gt;This can help users and operating system developers to collaboratively debug hardware related issues, check for operating system compatibility and find drivers.&lt;/p&gt;
         &lt;p&gt;You will get a permanent probe URL to view and share collected information.&lt;/p&gt;&lt;br&gt;&lt;br&gt;&lt;br&gt;</source>
-        <translation type="unfinished"></translation>
+        <translation>&lt;p&gt; 이 응용 프로그램은 컴퓨터 하드웨어에 대한 세부 정보를 수집하고 익명으로 공용 데이터베이스에 업로드할 수 있습니다. &lt;/p&gt;
+        &lt;p&gt; 이를 통해 사용자와 운영 체제 개발자는 하드웨어 관련 문제를 감지하고 운영 체제 호환성을 확인하고 드라이버를 찾을 수 있습니다. &lt;/p&gt;
+        &lt;p&gt; 수집된 콘텐츠를 보고 공유할 수 있는 영구 검색 URL을 받게 됩니다. &lt;/p&gt;&lt;br&gt;&lt;br&gt;&lt;br&gt;</translation>
     </message>
     <message>
         <source>Show Hardware Probe</source>
-        <translation type="unfinished"></translation>
+        <translation>하드웨어 감지 표시합니다</translation>
     </message>
     <message>
         <source>Failed to open the hardware probe.</source>
-        <translation type="unfinished"></translation>
+        <translation>하드웨어 감지를 켤 수 없습니다.</translation>
     </message>
     <message>
         <source>Failed to run the %s tool.</source>
-        <translation type="unfinished"></translation>
+        <translation>%s 도구를 실행할 수 없습니다.</translation>
     </message>
     <message>
         <source>Failed to set the owner to %x.</source>
-        <translation type="unfinished"></translation>
+        <translation>소유자를 %x으로 설정할 수 없습니다.</translation>
     </message>
     <message>
         <source>Uploading Hardware Probe</source>
-        <translation type="unfinished"></translation>
+        <translation>하드웨어 정찰이 업로드되고 있습니다</translation>
     </message>
     <message>
         <source>The Hardware Probe is being uploaded to the public database</source>
-        <translation type="unfinished"></translation>
+        <translation>하드웨어 검색이 공용 데이터베이스에 업로드되고 있습니다</translation>
     </message>
     <message>
         <source>You need an active internet connection in order to upload.</source>
-        <translation type="unfinished"></translation>
+        <translation>업로드하려면 사용 가능한 인터넷에 연결되어 있어야 합니다.</translation>
     </message>
     <message>
         <source>Failed to upload using the %s tool.</source>
-        <translation type="unfinished"></translation>
+        <translation>%s 도구를 사용하는 동안 업로드에 실패했습니다.</translation>
     </message>
     <message>
         <source>Hardware Probe Uploaded</source>
-        <translation type="unfinished"></translation>
+        <translation>하드웨어 정찰이 업로드되었습니다</translation>
     </message>
     <message>
         <source>Quit</source>
-        <translation type="unfinished"></translation>
+        <translation>종료합니다</translation>
     </message>
     <message>
         <source>Failed to open the uploaded hardware probe.</source>
-        <translation type="unfinished"></translation>
+        <translation>업로드된 하드웨어 감지를 켤 수 없습니다.</translation>
     </message>
     <message>
         <source>Error</source>
-        <translation type="unfinished"></translation>
+        <translation>오류</translation>
     </message>
     <message>
         <source>Hardware Probe was not successful.</source>
-        <translation type="unfinished"></translation>
+        <translation>하드웨어 정찰에 실패했습니다.</translation>
     </message>
 </context>
 <context>
     <name>tstr</name>
     <message>
         <source>Hello World</source>
-        <translation type="unfinished"></translation>
+        <translation>세상아, 안녕</translation>
     </message>
 </context>
 </TS>
