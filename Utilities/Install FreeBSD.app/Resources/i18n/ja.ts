f7f433e423da44d1546d41b3e727e88b85f9bf9b
louies0623 <louies0623@gmail.com>
2021-10-10 10:22:37 +0000

    Translated using Weblate (Japanese)
    Currently translated at 1.9% (1 of 51 strings)
    
    Translation: helloSystem/Install FreeBSD
    Translate-URL: https://hosted.weblate.org/projects/hellosystem/install/ja/

--- Utilities/Install FreeBSD.app/Resources/i18n/ja.ts
+++ Utilities/Install FreeBSD.app/Resources/i18n/ja.ts
@@ -208,7 +208,7 @@
     <name>tstr</name>
     <message>
         <source>Hello World</source>
-        <translation type="unfinished"></translation>
+        <translation>ハローワールド</translation>
     </message>
 </context>
 </TS>
