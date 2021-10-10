832321f86778605fd9fb1ec6eeef86aac6ecc0c5
louies0623 <louies0623@gmail.com>
2021-10-10 09:30:23 +0000

    Translated using Weblate (Korean)
    Currently translated at 100.0% (29 of 29 strings)
    
    Translation: helloSystem/Create Live Media
    Translate-URL: https://hosted.weblate.org/projects/hellosystem/create-live-media/ko/

--- Utilities/Create Live Media.app/Resources/i18n/ko.ts
+++ Utilities/Create Live Media.app/Resources/i18n/ko.ts
@@ -5,122 +5,122 @@
     <name>@default</name>
     <message>
         <source>Create Live Media</source>
-        <translation type="unfinished"></translation>
+        <translation>라이브 미디어 만들기</translation>
     </message>
     <message>
         <source>This will download a Live image and will write it to an attached storage device.</source>
-        <translation type="unfinished"></translation>
+        <translation>이렇게 하면 라이브 이미지가 다운로드되고 연결된 저장 장치에 기록됩니다.</translation>
     </message>
     <message>
         <source>Other...</source>
-        <translation type="unfinished"></translation>
+        <translation>다른...</translation>
     </message>
     <message>
         <source>Local ISO file...</source>
-        <translation type="unfinished"></translation>
+        <translation>로컬 ISO 파일...</translation>
     </message>
     <message>
         <source>Please choose an image:</source>
-        <translation type="unfinished"></translation>
+        <translation>이미지를 선택하십시오:</translation>
     </message>
     <message>
         <source>Date</source>
-        <translation type="unfinished"></translation>
+        <translation>날짜</translation>
     </message>
     <message>
         <source>Show Pre-release builds</source>
-        <translation type="unfinished"></translation>
+        <translation>미리 보기 버전을 표시합니다</translation>
     </message>
     <message>
         <source>Other</source>
-        <translation type="unfinished"></translation>
+        <translation>다른</translation>
     </message>
     <message>
         <source>URL of the ISO</source>
-        <translation type="unfinished"></translation>
+        <translation>ISO 의 URL</translation>
     </message>
     <message>
         <source>Disk images (*.iso *.img);;All files (*.*)</source>
-        <translation type="unfinished"></translation>
+        <translation>디스크 이미지 (*.iso*.img);; 모든 파일 (*.*)</translation>
     </message>
     <message>
         <source>This requires an active internet connection.</source>
-        <translation type="unfinished"></translation>
+        <translation>이렇게 하려면 인터넷 연결이 필요합니다.</translation>
     </message>
     <message>
         <source>Select Destination Disk</source>
-        <translation type="unfinished"></translation>
+        <translation>대상 디스크를 선택합니다</translation>
     </message>
     <message>
         <source>All data on the selected disk will be erased.</source>
-        <translation type="unfinished"></translation>
+        <translation>선택한 디스크의 모든 데이터가 지워집니다.</translation>
     </message>
     <message>
         <source>Could not determine the required disk space.</source>
-        <translation type="unfinished"></translation>
+        <translation>필요한 디스크 공간을 확인할 수 없습니다.</translation>
     </message>
     <message>
         <source>Disk space required: %s MiB</source>
-        <translation type="unfinished"></translation>
+        <translation>디스크 공간 필요 : %s MiB</translation>
     </message>
     <message>
         <source>Warning</source>
-        <translation type="unfinished"></translation>
+        <translation>경고</translation>
     </message>
     <message>
         <source>This will erase all contents of this disk and install the live system on it. Continue?</source>
-        <translation type="unfinished"></translation>
+        <translation>이렇게 하면 이 디스크의 모든 내용을 지우고 라이브 시스템을 설치합니다. 계속하다?</translation>
     </message>
     <message>
         <source>Downloading and writing Live medium</source>
-        <translation type="unfinished"></translation>
+        <translation>라이브 매체 다운로드 및 작성</translation>
     </message>
     <message>
         <source>The Live image is being downloaded and written to the medium.</source>
-        <translation type="unfinished"></translation>
+        <translation>라이브 이미지가 다운로드되어 매체에 기록됩니다.</translation>
     </message>
     <message>
         <source>Could not unmount parititons.</source>
-        <translation type="unfinished"></translation>
+        <translation>분할을 언로드할 수 없습니다.</translation>
     </message>
     <message>
         <source>An error occured while trying to write the image. Is the download URL accessible? Were all partitions unmounted? Do you have write permissions there?</source>
-        <translation type="unfinished"></translation>
+        <translation>이미지를 작성하려고 할 때 오류가 발생했습니다. 다운로드 URL에 액세스할 수 있습니까? 모든 파티션을 마운트 해제한 것입니까? 거기에 쓰기 권한이 있습니까?</translation>
     </message>
     <message>
         <source>Live Medium Complete</source>
-        <translation type="unfinished"></translation>
+        <translation>라이브 미디엄 완료</translation>
     </message>
     <message>
         <source>The Live image has been written to the device.</source>
-        <translation type="unfinished"></translation>
+        <translation>라이브 이미지가 장치에 기록되었습니다.</translation>
     </message>
     <message>
         <source>You can now start your computer from the Live medium.</source>
-        <translation type="unfinished"></translation>
+        <translation>이제 Live 매체에서 컴퓨터를 시작할 수 있습니다.</translation>
     </message>
     <message>
         <source>Quit</source>
-        <translation type="unfinished"></translation>
+        <translation>종료합니다</translation>
     </message>
     <message>
         <source>Device was unplugged, exiting</source>
-        <translation type="unfinished"></translation>
+        <translation>장치가 분리되어 종료되었습니다</translation>
     </message>
     <message>
         <source>Error</source>
-        <translation type="unfinished"></translation>
+        <translation>오류</translation>
     </message>
     <message>
         <source>The installation could not be performed.</source>
-        <translation type="unfinished"></translation>
+        <translation>설치를 수행할 수 없습니다.</translation>
     </message>
 </context>
 <context>
     <name>tstr</name>
     <message>
         <source>Hello World</source>
-        <translation type="unfinished"></translation>
+        <translation>세상아, 안녕</translation>
     </message>
 </context>
 </TS>
