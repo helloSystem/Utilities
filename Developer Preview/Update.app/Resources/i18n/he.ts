6098fba74f650326f7a1bd2c7d4fb0a61bdfcda2
Yaron Shahrabani <sh.yaron@gmail.com>
2021-10-20 19:14:18 +0000

    Translated using Weblate (Hebrew)
    Currently translated at 100.0% (14 of 14 strings)
    
    Translation: helloSystem/Update
    Translate-URL: https://hosted.weblate.org/projects/hellosystem/update/he/

--- Developer Preview/Update.app/Resources/i18n/he.ts
+++ Developer Preview/Update.app/Resources/i18n/he.ts
@@ -5,59 +5,59 @@
     <name>@default</name>
     <message>
         <source>This requires an active internet connection.</source>
-        <translation type="unfinished"></translation>
+        <translation>דורש חיבור פעיל לאינטרנט.</translation>
     </message>
     <message>
         <source>%s needs to be downloaded before it can be used.</source>
-        <translation type="unfinished"></translation>
+        <translation>יש להוריד את %s לפני שאפשר יהיה להשתמש בו.</translation>
     </message>
     <message>
         <source>Do you want to download it now?</source>
-        <translation type="unfinished"></translation>
+        <translation>להוריד אותו כעת?</translation>
     </message>
     <message>
         <source>The following packages and their dependencies be installed:</source>
-        <translation type="unfinished"></translation>
+        <translation>החבילות הבאות והתלויות שלהן יותקנו:</translation>
     </message>
     <message>
         <source>This will update FreeBSD and all installed packages.</source>
-        <translation type="unfinished"></translation>
+        <translation>פעולה זו תעדכן את FreeBSD ואת כל החבילות המותקנות.</translation>
     </message>
     <message>
         <source>Do you want to update the system now?</source>
-        <translation type="unfinished"></translation>
+        <translation>לעדכן את המערכת כעת?</translation>
     </message>
     <message>
         <source>Update</source>
-        <translation type="unfinished"></translation>
+        <translation>לעדכן</translation>
     </message>
     <message>
         <source>Cancel</source>
-        <translation type="unfinished"></translation>
+        <translation>לבטל</translation>
     </message>
     <message>
         <source>%s cannot be started.</source>
-        <translation type="unfinished"></translation>
+        <translation>אי אפשר להפעיל את %s.</translation>
     </message>
     <message>
         <source>Installing...</source>
-        <translation type="unfinished"></translation>
+        <translation>מתבצעת התקנה…</translation>
     </message>
     <message>
         <source>Your system is up to date.</source>
-        <translation type="unfinished"></translation>
+        <translation>המערכת שלך עדכנית.</translation>
     </message>
     <message>
         <source>Updating FreeBSD Packages...</source>
-        <translation type="unfinished"></translation>
+        <translation>חבילות FreeBSD מתעדכנות…</translation>
     </message>
     <message>
         <source>Downloading %s...</source>
-        <translation type="unfinished"></translation>
+        <translation>%s מתקבלת…</translation>
     </message>
     <message>
         <source>Updating FreeBSD...</source>
-        <translation type="unfinished"></translation>
+        <translation>FreeBSD מעודכן…</translation>
     </message>
 </context>
 </TS>
