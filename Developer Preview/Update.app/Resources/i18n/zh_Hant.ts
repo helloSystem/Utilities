b874853c55d41dee0160208fa38ab5657d96c778
probonopd <probono@puredarwin.org>
2021-10-20 20:23:10 +0200

    Added translation using Weblate (Chinese (Traditional))

---
+++ Developer Preview/Update.app/Resources/i18n/zh_Hant.ts
@@ -0,0 +1,63 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="zh_Hant">
+<context>
+    <name>@default</name>
+    <message>
+        <source>This requires an active internet connection.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>%s needs to be downloaded before it can be used.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Do you want to download it now?</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>The following packages and their dependencies be installed:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>This will update FreeBSD and all installed packages.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Do you want to update the system now?</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Update</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Cancel</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>%s cannot be started.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Installing...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Your system is up to date.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Updating FreeBSD Packages...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Downloading %s...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Updating FreeBSD...</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
