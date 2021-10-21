30a43d5b8d53190b9e5eebe84a42dbabb3b7272e
Emir <emir_sari@msn.com>
2021-10-21 15:08:14 +0000

    Translated using Weblate (Turkish)
    Currently translated at 100.0% (29 of 29 strings)
    
    Translation: helloSystem/Create Live Media
    Translate-URL: https://hosted.weblate.org/projects/hellosystem/create-live-media/tr/

--- Utilities/Create Live Media.app/Resources/i18n/tr.ts
+++ Utilities/Create Live Media.app/Resources/i18n/tr.ts
@@ -5,122 +5,122 @@
     <name>@default</name>
     <message>
         <source>Create Live Media</source>
-        <translation type="unfinished"></translation>
+        <translation>Canlı disk kalıbı oluştur</translation>
     </message>
     <message>
         <source>This will download a Live image and will write it to an attached storage device.</source>
-        <translation type="unfinished"></translation>
+        <translation>Bu işlem, bir canlı disk kalıbı indirip bağlı bir depolama aygıtına yazacaktır.</translation>
     </message>
     <message>
         <source>Other...</source>
-        <translation type="unfinished"></translation>
+        <translation>Diğer…</translation>
     </message>
     <message>
         <source>Local ISO file...</source>
-        <translation type="unfinished"></translation>
+        <translation>Yerel ISO dosyası…</translation>
     </message>
     <message>
         <source>Please choose an image:</source>
-        <translation type="unfinished"></translation>
+        <translation>Bir kalıp seçin:</translation>
     </message>
     <message>
         <source>Date</source>
-        <translation type="unfinished"></translation>
+        <translation>Tarih</translation>
     </message>
     <message>
         <source>Show Pre-release builds</source>
-        <translation type="unfinished"></translation>
+        <translation>Kararlı olmayan yapıları göster</translation>
     </message>
     <message>
         <source>Other</source>
-        <translation type="unfinished"></translation>
+        <translation>Diğer</translation>
     </message>
     <message>
         <source>URL of the ISO</source>
-        <translation type="unfinished"></translation>
+        <translation>ISO URL&apos;si</translation>
     </message>
     <message>
         <source>Disk images (*.iso *.img);;All files (*.*)</source>
-        <translation type="unfinished"></translation>
+        <translation>Disk kalıpları (*.iso, *.img);;Tüm dosyalar(*.*)</translation>
     </message>
     <message>
         <source>This requires an active internet connection.</source>
-        <translation type="unfinished"></translation>
+        <translation>Bu işlem, bir internet bağlantısı gerektirir.</translation>
     </message>
     <message>
         <source>Select Destination Disk</source>
-        <translation type="unfinished"></translation>
+        <translation>Hedef disk seçin</translation>
     </message>
     <message>
         <source>All data on the selected disk will be erased.</source>
-        <translation type="unfinished"></translation>
+        <translation>Seçili diskteki tüm veri silinecektir.</translation>
     </message>
     <message>
         <source>Could not determine the required disk space.</source>
-        <translation type="unfinished"></translation>
+        <translation>Gerekli disk alanı hesaplanamıyor.</translation>
     </message>
     <message>
         <source>Disk space required: %s MiB</source>
-        <translation type="unfinished"></translation>
+        <translation>Gerekli disk alanı: %s MiB</translation>
     </message>
     <message>
         <source>Warning</source>
-        <translation type="unfinished"></translation>
+        <translation>Uyarı</translation>
     </message>
     <message>
         <source>This will erase all contents of this disk and install the live system on it. Continue?</source>
-        <translation type="unfinished"></translation>
+        <translation>Bu işlem, diskin tüm içeriğini silecek ve üzerine canlı sistemi kuracaktır. Sürdürülsün mü?</translation>
     </message>
     <message>
         <source>Downloading and writing Live medium</source>
-        <translation type="unfinished"></translation>
+        <translation>Canlı disk kalıbı indiriliyor ve yazılıyor</translation>
     </message>
     <message>
         <source>The Live image is being downloaded and written to the medium.</source>
-        <translation type="unfinished"></translation>
+        <translation>Canlı disk kalıbı şu anda indiriliyor ve ortama yazılıyor.</translation>
     </message>
     <message>
         <source>Could not unmount parititons.</source>
-        <translation type="unfinished"></translation>
+        <translation>Bölümlerin bağlantısı kesilemedi.</translation>
     </message>
     <message>
         <source>An error occured while trying to write the image. Is the download URL accessible? Were all partitions unmounted? Do you have write permissions there?</source>
-        <translation type="unfinished"></translation>
+        <translation>Kalıbı yazmaya çalışırken bir hata oluştu. İndirme URL&apos;si erişilebilir mi? Tüm bölümlerin bağlantısı kesili mi? Yazma erişiminiz var mı?</translation>
     </message>
     <message>
         <source>Live Medium Complete</source>
-        <translation type="unfinished"></translation>
+        <translation>Canlı ortam tamamlandı</translation>
     </message>
     <message>
         <source>The Live image has been written to the device.</source>
-        <translation type="unfinished"></translation>
+        <translation>Canlı disk kalıbı aygıta yazıldı.</translation>
     </message>
     <message>
         <source>You can now start your computer from the Live medium.</source>
-        <translation type="unfinished"></translation>
+        <translation>Artık bilgisayarınızı canlı ortamdan başlatabilirsiniz.</translation>
     </message>
     <message>
         <source>Quit</source>
-        <translation type="unfinished"></translation>
+        <translation>Çık</translation>
     </message>
     <message>
         <source>Device was unplugged, exiting</source>
-        <translation type="unfinished"></translation>
+        <translation>Aygıt bağlantısı kesildi, çıkılıyor</translation>
     </message>
     <message>
         <source>Error</source>
-        <translation type="unfinished"></translation>
+        <translation>Hata</translation>
     </message>
     <message>
         <source>The installation could not be performed.</source>
-        <translation type="unfinished"></translation>
+        <translation>Yükleme gerçekleştirilemedi.</translation>
     </message>
 </context>
 <context>
     <name>tstr</name>
     <message>
         <source>Hello World</source>
-        <translation type="unfinished"></translation>
+        <translation>Merhaba Dünya</translation>
     </message>
 </context>
 </TS>
