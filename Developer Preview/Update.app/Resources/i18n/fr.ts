99c6eb15b02fa108aa220b1d4fa71675c1185939
J. Lavoie <j.lavoie@net-c.ca>
2021-10-21 23:56:30 +0000

    Translated using Weblate (French)
    Currently translated at 100.0% (14 of 14 strings)
    
    Translation: helloSystem/Update
    Translate-URL: https://hosted.weblate.org/projects/hellosystem/update/fr/

--- Developer Preview/Update.app/Resources/i18n/fr.ts
+++ Developer Preview/Update.app/Resources/i18n/fr.ts
@@ -5,59 +5,59 @@
     <name>@default</name>
     <message>
         <source>This requires an active internet connection.</source>
-        <translation type="unfinished"></translation>
+        <translation>Ceci nécessite une connexion internet active.</translation>
     </message>
     <message>
         <source>%s needs to be downloaded before it can be used.</source>
-        <translation type="unfinished"></translation>
+        <translation>%s doit être téléchargé avant de pouvoir être utilisé.</translation>
     </message>
     <message>
         <source>Do you want to download it now?</source>
-        <translation type="unfinished"></translation>
+        <translation>Voulez-vous le télécharger maintenant&#xa0;?</translation>
     </message>
     <message>
         <source>The following packages and their dependencies be installed:</source>
-        <translation type="unfinished"></translation>
+        <translation>Les paquets suivants et leurs dépendances doivent être installés&#xa0;:</translation>
     </message>
     <message>
         <source>This will update FreeBSD and all installed packages.</source>
-        <translation type="unfinished"></translation>
+        <translation>Ceci mettra à jour FreeBSD et tous les paquets installés.</translation>
     </message>
     <message>
         <source>Do you want to update the system now?</source>
-        <translation type="unfinished"></translation>
+        <translation>Voulez-vous mettre à jour le système maintenant&#xa0;?</translation>
     </message>
     <message>
         <source>Update</source>
-        <translation type="unfinished"></translation>
+        <translation>Mettre à jour</translation>
     </message>
     <message>
         <source>Cancel</source>
-        <translation type="unfinished"></translation>
+        <translation>Annuler</translation>
     </message>
     <message>
         <source>%s cannot be started.</source>
-        <translation type="unfinished"></translation>
+        <translation>%s ne peut pas être lancé.</translation>
     </message>
     <message>
         <source>Installing...</source>
-        <translation type="unfinished"></translation>
+        <translation>Installation…</translation>
     </message>
     <message>
         <source>Your system is up to date.</source>
-        <translation type="unfinished"></translation>
+        <translation>Votre système est à jour.</translation>
     </message>
     <message>
         <source>Updating FreeBSD Packages...</source>
-        <translation type="unfinished"></translation>
+        <translation>Mise à jour des paquets FreeBSD…</translation>
     </message>
     <message>
         <source>Downloading %s...</source>
-        <translation type="unfinished"></translation>
+        <translation>Téléchargement de %s…</translation>
     </message>
     <message>
         <source>Updating FreeBSD...</source>
-        <translation type="unfinished"></translation>
+        <translation>Mise à jour de FreeBSD…</translation>
     </message>
 </context>
 </TS>
