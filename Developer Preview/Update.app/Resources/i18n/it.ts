459ddade7c70766f440f61350452395699be4ffd
J. Lavoie <j.lavoie@net-c.ca>
2021-10-22 00:12:11 +0000

    Translated using Weblate (Italian)
    Currently translated at 100.0% (14 of 14 strings)
    
    Translation: helloSystem/Update
    Translate-URL: https://hosted.weblate.org/projects/hellosystem/update/it/

--- Developer Preview/Update.app/Resources/i18n/it.ts
+++ Developer Preview/Update.app/Resources/i18n/it.ts
@@ -5,59 +5,59 @@
     <name>@default</name>
     <message>
         <source>This requires an active internet connection.</source>
-        <translation type="unfinished"></translation>
+        <translation>Questo richiede una connessione internet attiva.</translation>
     </message>
     <message>
         <source>%s needs to be downloaded before it can be used.</source>
-        <translation type="unfinished"></translation>
+        <translation>%s deve essere scaricato prima di poter essere utilizzato.</translation>
     </message>
     <message>
         <source>Do you want to download it now?</source>
-        <translation type="unfinished"></translation>
+        <translation>Vuoi scaricarlo ora?</translation>
     </message>
     <message>
         <source>The following packages and their dependencies be installed:</source>
-        <translation type="unfinished"></translation>
+        <translation>I seguenti pacchetti e le loro dipendenze devono essere installati:</translation>
     </message>
     <message>
         <source>This will update FreeBSD and all installed packages.</source>
-        <translation type="unfinished"></translation>
+        <translation>Questo aggiornerà FreeBSD e tutti i pacchetti installati.</translation>
     </message>
     <message>
         <source>Do you want to update the system now?</source>
-        <translation type="unfinished"></translation>
+        <translation>Vuoi aggiornare il sistema adesso?</translation>
     </message>
     <message>
         <source>Update</source>
-        <translation type="unfinished"></translation>
+        <translation>Aggiorna</translation>
     </message>
     <message>
         <source>Cancel</source>
-        <translation type="unfinished"></translation>
+        <translation>Annulla</translation>
     </message>
     <message>
         <source>%s cannot be started.</source>
-        <translation type="unfinished"></translation>
+        <translation>%s non può essere avviato.</translation>
     </message>
     <message>
         <source>Installing...</source>
-        <translation type="unfinished"></translation>
+        <translation>Installazione…</translation>
     </message>
     <message>
         <source>Your system is up to date.</source>
-        <translation type="unfinished"></translation>
+        <translation>Il tuo sistema è aggiornato.</translation>
     </message>
     <message>
         <source>Updating FreeBSD Packages...</source>
-        <translation type="unfinished"></translation>
+        <translation>Aggiornamento dei pacchetti FreeBSD…</translation>
     </message>
     <message>
         <source>Downloading %s...</source>
-        <translation type="unfinished"></translation>
+        <translation>Scaricamento di %s…</translation>
     </message>
     <message>
         <source>Updating FreeBSD...</source>
-        <translation type="unfinished"></translation>
+        <translation>Aggiornamento di FreeBSD…</translation>
     </message>
 </context>
 </TS>
