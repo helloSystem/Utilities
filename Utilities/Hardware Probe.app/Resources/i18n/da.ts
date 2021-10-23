6c24ab987a0d2d2fc417c525ee1ca76065f6f98a
Lars Henrik Ørn <larshenrikoern@outlook.dk>
2021-10-23 10:07:25 +0000

    Translated using Weblate (Danish)
    Currently translated at 100.0% (20 of 20 strings)
    
    Translation: helloSystem/Hardware Probe
    Translate-URL: https://hosted.weblate.org/projects/hellosystem/hardware-probe/da/

--- Utilities/Hardware Probe.app/Resources/i18n/da.ts
+++ Utilities/Hardware Probe.app/Resources/i18n/da.ts
@@ -5,88 +5,90 @@
     <name>@default</name>
     <message>
         <source>An unknown error occured.</source>
-        <translation type="unfinished"></translation>
+        <translation>Der opstod en ukendt fejl.</translation>
     </message>
     <message>
         <source>Hardware Probe</source>
-        <translation type="unfinished"></translation>
+        <translation>Tester hardware</translation>
     </message>
     <message>
         <source>Privacy Information</source>
-        <translation type="unfinished"></translation>
+        <translation>Information om Privacy</translation>
     </message>
     <message>
         <source>Uploading a Hardware Probe is subject to the following Pricacy Terms.</source>
-        <translation type="unfinished"></translation>
+        <translation>Uploading af hardware testen foregår efter følgende privatlivs betingelser.</translation>
     </message>
     <message>
         <source>Please see %s for more information.</source>
-        <translation type="unfinished"></translation>
+        <translation>Se venligst %s for mere information.</translation>
     </message>
     <message>
         <source>&lt;p&gt;This utility collects hardware details of your computer and can anonymously upload them to a public database.&lt;/p&gt;
         &lt;p&gt;This can help users and operating system developers to collaboratively debug hardware related issues, check for operating system compatibility and find drivers.&lt;/p&gt;
         &lt;p&gt;You will get a permanent probe URL to view and share collected information.&lt;/p&gt;&lt;br&gt;&lt;br&gt;&lt;br&gt;</source>
-        <translation type="unfinished"></translation>
+        <translation>&lt;p&gt;Dette program samler hardware information om din computer og kan sende dem annonymt til en offentlig database.&lt;/p&gt;
+        &lt;p&gt;Dette kan hjælpe brugere og system udvikleres til at finde hardware fejl. og problemer, kontrollere operativ system kompabilitet og finde drivere.&lt;/p&gt;
+        &lt;p&gt;Du vil få en fast URL til at se og dele den indsamlede.&lt;/p&gt;&lt;br&gt;&lt;br&gt;&lt;br&gt;</translation>
     </message>
     <message>
         <source>Show Hardware Probe</source>
-        <translation type="unfinished"></translation>
+        <translation>Vis hardware test resultat</translation>
     </message>
     <message>
         <source>Failed to open the hardware probe.</source>
-        <translation type="unfinished"></translation>
+        <translation>Kunne ikke teste hardware.</translation>
     </message>
     <message>
         <source>Failed to run the %s tool.</source>
-        <translation type="unfinished"></translation>
+        <translation>Kunne ikke køre %s værktøjet.</translation>
     </message>
     <message>
         <source>Failed to set the owner to %x.</source>
-        <translation type="unfinished"></translation>
+        <translation>Kunne ikke tildele brugeren til %x.</translation>
     </message>
     <message>
         <source>Uploading Hardware Probe</source>
-        <translation type="unfinished"></translation>
+        <translation>Oploader hardware test</translation>
     </message>
     <message>
         <source>The Hardware Probe is being uploaded to the public database</source>
-        <translation type="unfinished"></translation>
+        <translation>Hardware testen bliver uploded til den offentlige database</translation>
     </message>
     <message>
         <source>You need an active internet connection in order to upload.</source>
-        <translation type="unfinished"></translation>
+        <translation>Du skal have en aktiv internet forbindelse for at kunne uploade.</translation>
     </message>
     <message>
         <source>Failed to upload using the %s tool.</source>
-        <translation type="unfinished"></translation>
+        <translation>Kunne ikke uploade ved brug af %s værktøjet.</translation>
     </message>
     <message>
         <source>Hardware Probe Uploaded</source>
-        <translation type="unfinished"></translation>
+        <translation>Hardware test uploadet</translation>
     </message>
     <message>
         <source>Quit</source>
-        <translation type="unfinished"></translation>
+        <translation>Afslut</translation>
     </message>
     <message>
         <source>Failed to open the uploaded hardware probe.</source>
-        <translation type="unfinished"></translation>
+        <translation>Kunne ikke uploade hardware testen.</translation>
     </message>
     <message>
         <source>Error</source>
-        <translation type="unfinished"></translation>
+        <translation>Fejl</translation>
     </message>
     <message>
         <source>Hardware Probe was not successful.</source>
-        <translation type="unfinished"></translation>
+        <translation>Hardware testen mislykkedes.</translation>
     </message>
 </context>
 <context>
     <name>tstr</name>
     <message>
         <source>Hello World</source>
-        <translation type="unfinished"></translation>
+        <translation>Hello World</translation>
     </message>
 </context>
 </TS>
