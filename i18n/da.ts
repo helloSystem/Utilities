67726547d6906c1cfdc1298014a22c745a8cd4cb
Lars Henrik Ørn <larshenrikoern@outlook.dk>
2021-10-24 12:46:09 +0000

    Translated using Weblate (Danish)
    Currently translated at 100.0% (23 of 23 strings)
    
    Translation: helloSystem/Utilities
    Translate-URL: https://hosted.weblate.org/projects/hellosystem/utilities/da/

--- i18n/da.ts
+++ i18n/da.ts
@@ -96,7 +96,7 @@
     <name>main</name>
     <message>
         <source>Hello World</source>
-        <translation>Hello World</translation>
+        <translation>Hej Verden</translation>
     </message>
     <message>
         <source>Text Edit</source>
