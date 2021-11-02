f875e6246bf0f16f3eadd5d791330c12e87589ec
Allan Nordhøy <epost@anotheragency.no>
2021-11-02 01:02:09 +0000

    Translated using Weblate (Norwegian Bokmål)
    Currently translated at 100.0% (14 of 14 strings)
    
    Translation: helloSystem/Update
    Translate-URL: https://hosted.weblate.org/projects/hellosystem/update/nb_NO/

--- Developer Preview/Update.app/Resources/i18n/nb_NO.ts
+++ Developer Preview/Update.app/Resources/i18n/nb_NO.ts
@@ -5,59 +5,59 @@
     <name>@default</name>
     <message>
         <source>This requires an active internet connection.</source>
-        <translation type="unfinished"></translation>
+        <translation>Dette krever en aktiv internettforbindelse.</translation>
     </message>
     <message>
         <source>%s needs to be downloaded before it can be used.</source>
-        <translation type="unfinished"></translation>
+        <translation>%s må lastes ned før dent kan brukes.</translation>
     </message>
     <message>
         <source>Do you want to download it now?</source>
-        <translation type="unfinished"></translation>
+        <translation>Ønsker du å laste det ned nå?</translation>
     </message>
     <message>
         <source>The following packages and their dependencies be installed:</source>
-        <translation type="unfinished"></translation>
+        <translation>Følgende pakker og avhengigheter vil bli installert:</translation>
     </message>
     <message>
         <source>This will update FreeBSD and all installed packages.</source>
-        <translation type="unfinished"></translation>
+        <translation>Dette vil oppgradere FreeBSD og alle installerte pakker.</translation>
     </message>
     <message>
         <source>Do you want to update the system now?</source>
-        <translation type="unfinished"></translation>
+        <translation>Vil du oppgradere systemet nå?</translation>
     </message>
     <message>
         <source>Update</source>
-        <translation type="unfinished"></translation>
+        <translation>Oppgrader</translation>
     </message>
     <message>
         <source>Cancel</source>
-        <translation type="unfinished"></translation>
+        <translation>Avbryt</translation>
     </message>
     <message>
         <source>%s cannot be started.</source>
-        <translation type="unfinished"></translation>
+        <translation>%s kan ikke startes.</translation>
     </message>
     <message>
         <source>Installing...</source>
-        <translation type="unfinished"></translation>
+        <translation>Installerer …</translation>
     </message>
     <message>
         <source>Your system is up to date.</source>
-        <translation type="unfinished"></translation>
+        <translation>Systemet ditt er oppgradert.</translation>
     </message>
     <message>
         <source>Updating FreeBSD Packages...</source>
-        <translation type="unfinished"></translation>
+        <translation>Oppgraderer FreeBSD-pakker …</translation>
     </message>
     <message>
         <source>Downloading %s...</source>
-        <translation type="unfinished"></translation>
+        <translation>Laster ned %s …</translation>
     </message>
     <message>
         <source>Updating FreeBSD...</source>
-        <translation type="unfinished"></translation>
+        <translation>Oppgraderer FreeBSD …</translation>
     </message>
 </context>
 </TS>
