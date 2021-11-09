65b2e13a6b2cdc1a27d5a4e662225ed08f42cbcf
Taras Panchenko <top39@ukr.net>
2021-11-09 10:13:40 +0000

    Translated using Weblate (Ukrainian)
    Currently translated at 100.0% (20 of 20 strings)
    
    Translation: helloSystem/Hardware Probe
    Translate-URL: https://hosted.weblate.org/projects/hellosystem/hardware-probe/uk/

--- Utilities/Hardware Probe.app/Resources/i18n/uk.ts
+++ Utilities/Hardware Probe.app/Resources/i18n/uk.ts
@@ -9,61 +9,63 @@
     </message>
     <message>
         <source>Hardware Probe</source>
-        <translation type="unfinished"></translation>
+        <translation>Пошук апаратних засобів</translation>
     </message>
     <message>
         <source>Privacy Information</source>
-        <translation type="unfinished"></translation>
+        <translation>Інформація про конфіденційність</translation>
     </message>
     <message>
         <source>Uploading a Hardware Probe is subject to the following Pricacy Terms.</source>
-        <translation type="unfinished"></translation>
+        <translation>Завантаження результатів пошуку апаратних засобів регулюється наведеними нижче Умовами конфіденційності.</translation>
     </message>
     <message>
         <source>Please see %s for more information.</source>
-        <translation type="unfinished"></translation>
+        <translation>Будь ласка, перегляньте %s для отримання додаткової інформації.</translation>
     </message>
     <message>
         <source>&lt;p&gt;This utility collects hardware details of your computer and can anonymously upload them to a public database.&lt;/p&gt;
         &lt;p&gt;This can help users and operating system developers to collaboratively debug hardware related issues, check for operating system compatibility and find drivers.&lt;/p&gt;
         &lt;p&gt;You will get a permanent probe URL to view and share collected information.&lt;/p&gt;&lt;br&gt;&lt;br&gt;&lt;br&gt;</source>
-        <translation type="unfinished"></translation>
+        <translation>&lt;p&gt;Цей застосунок збирає відомості про апаратні засоби вашого комп’ютера та може анонімно завантажувати їх у загальнодоступну базу даних.&lt;/p&gt;
+        &lt;p&gt;Це може допомогти користувачам і розробникам операційної системи спільно вирішувати проблеми, пов’язані з обладнанням, перевіряти сумісність операційної системи та знаходити драйвери.&lt;/p&gt;
+        &lt;p&gt;Ви отримаєте постійне посилання (URL-адресу) на ваші результати для перегляду та обміну зібраною інформацією.&lt;/p&gt;&lt;br&gt;&lt;br&gt;&lt;br&gt;</translation>
     </message>
     <message>
         <source>Show Hardware Probe</source>
-        <translation type="unfinished"></translation>
+        <translation>Показати результати пошуку апаратних засобів</translation>
     </message>
     <message>
         <source>Failed to open the hardware probe.</source>
-        <translation type="unfinished"></translation>
+        <translation>Не вдалося відкрити результати пошуку апаратних засобів.</translation>
     </message>
     <message>
         <source>Failed to run the %s tool.</source>
-        <translation type="unfinished"></translation>
+        <translation>Не вдалося запустити %s.</translation>
     </message>
     <message>
         <source>Failed to set the owner to %x.</source>
-        <translation type="unfinished"></translation>
+        <translation>Не вдалося встановити власника для %x.</translation>
     </message>
     <message>
         <source>Uploading Hardware Probe</source>
-        <translation type="unfinished"></translation>
+        <translation>Завантаження результатів пошуку апаратних засобів</translation>
     </message>
     <message>
         <source>The Hardware Probe is being uploaded to the public database</source>
-        <translation type="unfinished"></translation>
+        <translation>Результати пошуку апаратних засобів завантажуються до загальнодоступної бази даних</translation>
     </message>
     <message>
         <source>You need an active internet connection in order to upload.</source>
-        <translation type="unfinished"></translation>
+        <translation>Для завантаження потрібне активне підключення до Інтернету.</translation>
     </message>
     <message>
         <source>Failed to upload using the %s tool.</source>
-        <translation type="unfinished"></translation>
+        <translation>Не вдалося завантажити за допомогою %s.</translation>
     </message>
     <message>
         <source>Hardware Probe Uploaded</source>
-        <translation type="unfinished"></translation>
+        <translation>Результати пошуку апаратних засобів завантажені</translation>
     </message>
     <message>
         <source>Quit</source>
@@ -71,7 +73,7 @@
     </message>
     <message>
         <source>Failed to open the uploaded hardware probe.</source>
-        <translation type="unfinished"></translation>
+        <translation>Не вдалося відкрити завантажені результати пошуку апаратних засобів.</translation>
     </message>
     <message>
         <source>Error</source>
@@ -79,7 +81,7 @@
     </message>
     <message>
         <source>Hardware Probe was not successful.</source>
-        <translation type="unfinished"></translation>
+        <translation>Не вдалий пошук апаратних засобів.</translation>
     </message>
 </context>
 <context>
