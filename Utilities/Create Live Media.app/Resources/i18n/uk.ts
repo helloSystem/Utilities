70500da9005edfc094d7468214a30f9cd05ec252
Taras Panchenko <top39@ukr.net>
2021-11-09 12:41:40 +0000

    Translated using Weblate (Ukrainian)
    Currently translated at 100.0% (29 of 29 strings)
    
    Translation: helloSystem/Create Live Media
    Translate-URL: https://hosted.weblate.org/projects/hellosystem/create-live-media/uk/

--- Utilities/Create Live Media.app/Resources/i18n/uk.ts
+++ Utilities/Create Live Media.app/Resources/i18n/uk.ts
@@ -5,43 +5,43 @@
     <name>@default</name>
     <message>
         <source>Create Live Media</source>
-        <translation type="unfinished"></translation>
+        <translation>Створити Live носій</translation>
     </message>
     <message>
         <source>This will download a Live image and will write it to an attached storage device.</source>
-        <translation type="unfinished"></translation>
+        <translation>Буде завантажено live-образ системи та записано на під&apos;єднаний носій пам’яті.</translation>
     </message>
     <message>
         <source>Other...</source>
-        <translation type="unfinished"></translation>
+        <translation>Інше...</translation>
     </message>
     <message>
         <source>Local ISO file...</source>
-        <translation type="unfinished"></translation>
+        <translation>Існуючий файл ISO...</translation>
     </message>
     <message>
         <source>Please choose an image:</source>
-        <translation type="unfinished"></translation>
+        <translation>Будь ласка, виберіть образ:</translation>
     </message>
     <message>
         <source>Date</source>
-        <translation type="unfinished"></translation>
+        <translation>Дата</translation>
     </message>
     <message>
         <source>Show Pre-release builds</source>
-        <translation type="unfinished"></translation>
+        <translation>Показати попередні до випуску збірки</translation>
     </message>
     <message>
         <source>Other</source>
-        <translation type="unfinished"></translation>
+        <translation>Інше</translation>
     </message>
     <message>
         <source>URL of the ISO</source>
-        <translation type="unfinished"></translation>
+        <translation>Посилання URL на ISO</translation>
     </message>
     <message>
         <source>Disk images (*.iso *.img);;All files (*.*)</source>
-        <translation type="unfinished"></translation>
+        <translation>Образи носіїв (*.iso *.img);;Всі файли (*.*)</translation>
     </message>
     <message>
         <source>This requires an active internet connection.</source>
@@ -57,7 +57,7 @@
     </message>
     <message>
         <source>Could not determine the required disk space.</source>
-        <translation type="unfinished"></translation>
+        <translation>Не вдалося визначити необхідний простір на носієві.</translation>
     </message>
     <message>
         <source>Disk space required: %s MiB</source>
@@ -69,35 +69,35 @@
     </message>
     <message>
         <source>This will erase all contents of this disk and install the live system on it. Continue?</source>
-        <translation type="unfinished"></translation>
+        <translation>Ця дія видалить весь вміст вказаного накопичувача та встановить на ньому поточну систему. Продовжити?</translation>
     </message>
     <message>
         <source>Downloading and writing Live medium</source>
-        <translation type="unfinished"></translation>
+        <translation>Завантаження та запис Live носія</translation>
     </message>
     <message>
         <source>The Live image is being downloaded and written to the medium.</source>
-        <translation type="unfinished"></translation>
+        <translation>Live образ завантажується та записується на носій.</translation>
     </message>
     <message>
         <source>Could not unmount parititons.</source>
-        <translation type="unfinished"></translation>
+        <translation>Не вдалося від&apos;єднати розділи.</translation>
     </message>
     <message>
         <source>An error occured while trying to write the image. Is the download URL accessible? Were all partitions unmounted? Do you have write permissions there?</source>
-        <translation type="unfinished"></translation>
+        <translation>Під час спроби записати образ сталася помилка. Чи доступна URL-адреса для завантаження? Чи всі розділи були від&apos;єднано? Чи є у вас дозвіл на запис?</translation>
     </message>
     <message>
         <source>Live Medium Complete</source>
-        <translation type="unfinished"></translation>
+        <translation>Завершено створення Live носія</translation>
     </message>
     <message>
         <source>The Live image has been written to the device.</source>
-        <translation type="unfinished"></translation>
+        <translation>Live образ було записано на пристрій.</translation>
     </message>
     <message>
         <source>You can now start your computer from the Live medium.</source>
-        <translation type="unfinished"></translation>
+        <translation>Тепер ви можете запустити комп’ютер із Live носія.</translation>
     </message>
     <message>
         <source>Quit</source>
@@ -105,7 +105,7 @@
     </message>
     <message>
         <source>Device was unplugged, exiting</source>
-        <translation type="unfinished"></translation>
+        <translation>Пристрій відключено, вихід</translation>
     </message>
     <message>
         <source>Error</source>
