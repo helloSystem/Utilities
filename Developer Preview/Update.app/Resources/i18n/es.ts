ca72d070d03768838227f781e3720d32dbe522a5
Alphamodh <alphamodh0@gmail.com>
2021-11-13 15:48:38 +0000

    Translated using Weblate (Spanish)
    Currently translated at 100.0% (14 of 14 strings)
    
    Translation: helloSystem/Update
    Translate-URL: https://hosted.weblate.org/projects/hellosystem/update/es/

--- Developer Preview/Update.app/Resources/i18n/es.ts
+++ Developer Preview/Update.app/Resources/i18n/es.ts
@@ -5,59 +5,59 @@
     <name>@default</name>
     <message>
         <source>This requires an active internet connection.</source>
-        <translation type="unfinished"></translation>
+        <translation>Se requiere una conexión activa a internet.</translation>
     </message>
     <message>
         <source>%s needs to be downloaded before it can be used.</source>
-        <translation type="unfinished"></translation>
+        <translation>%s necesita ser descargado antes de poder utilizarse.</translation>
     </message>
     <message>
         <source>Do you want to download it now?</source>
-        <translation type="unfinished"></translation>
+        <translation>¿Desea descargarlo ahora?</translation>
     </message>
     <message>
         <source>The following packages and their dependencies be installed:</source>
-        <translation type="unfinished"></translation>
+        <translation>Se instalarán los siguientes paquetes y sus dependencias:</translation>
     </message>
     <message>
         <source>This will update FreeBSD and all installed packages.</source>
-        <translation type="unfinished"></translation>
+        <translation>Se actualizará FreeBSD y todos los paquetes instalados.</translation>
     </message>
     <message>
         <source>Do you want to update the system now?</source>
-        <translation type="unfinished"></translation>
+        <translation>¿Desea aplicar la actualización a su sistema ahora?</translation>
     </message>
     <message>
         <source>Update</source>
-        <translation type="unfinished"></translation>
+        <translation>Actualizar</translation>
     </message>
     <message>
         <source>Cancel</source>
-        <translation type="unfinished"></translation>
+        <translation>Cancelar</translation>
     </message>
     <message>
         <source>%s cannot be started.</source>
-        <translation type="unfinished"></translation>
+        <translation>%s no puede iniciarse.</translation>
     </message>
     <message>
         <source>Installing...</source>
-        <translation type="unfinished"></translation>
+        <translation>Instalando...</translation>
     </message>
     <message>
         <source>Your system is up to date.</source>
-        <translation type="unfinished"></translation>
+        <translation>Su sistema está actualizado.</translation>
     </message>
     <message>
         <source>Updating FreeBSD Packages...</source>
-        <translation type="unfinished"></translation>
+        <translation>Actualizando los Paquetes de FreeBSD...</translation>
     </message>
     <message>
         <source>Downloading %s...</source>
-        <translation type="unfinished"></translation>
+        <translation>Descargando %s...</translation>
     </message>
     <message>
         <source>Updating FreeBSD...</source>
-        <translation type="unfinished"></translation>
+        <translation>Actualizando FreeBSD...</translation>
     </message>
 </context>
 </TS>
