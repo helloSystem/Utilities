e0108556d09249cc90f320e405fbf148dbd0bce1
Alphamodh <alphamodh0@gmail.com>
2021-11-13 15:19:42 +0000

    Translated using Weblate (Spanish)
    Currently translated at 100.0% (51 of 51 strings)
    
    Translation: helloSystem/Install FreeBSD
    Translate-URL: https://hosted.weblate.org/projects/hellosystem/install/es/

--- Utilities/Install FreeBSD.app/Resources/i18n/es.ts
+++ Utilities/Install FreeBSD.app/Resources/i18n/es.ts
@@ -5,210 +5,210 @@
     <name>@default</name>
     <message>
         <source>Install FreeBSD</source>
-        <translation type="unfinished"></translation>
+        <translation>Instalar FreeBSD</translation>
     </message>
     <message>
         <source>An unknown error occured.</source>
-        <translation type="unfinished"></translation>
+        <translation>Ocurrió un error desconocido.</translation>
     </message>
     <message>
         <source>Pre-existing %s removed</source>
-        <translation type="unfinished"></translation>
+        <translation>%s ha sido removido</translation>
     </message>
     <message>
         <source>Installer Log</source>
-        <translation type="unfinished"></translation>
+        <translation>Registro de Instalación</translation>
     </message>
     <message>
         <source>The Installer Log cannot be opened.</source>
-        <translation type="unfinished"></translation>
+        <translation>El Registro de Instalación no pudo abrirse.</translation>
     </message>
     <message>
         <source>Question</source>
-        <translation type="unfinished"></translation>
+        <translation>Pregunta</translation>
     </message>
     <message>
         <source>This will send a request to ipapi.com one time to locate your computer over the network. Continue?</source>
-        <translation type="unfinished"></translation>
+        <translation>Esto enviará una petición a ipapi.com una vez para localizar su equipo en la red. ¿Continuar?</translation>
     </message>
     <message>
         <source>The installer script was not found. Please make sure that you are running the installer on a supported system.</source>
-        <translation type="unfinished"></translation>
+        <translation>El script de instalación no se encontró. Por favor, asegúrese de que está iniciando el instalador en un sistema compatible.</translation>
     </message>
     <message>
         <source>The sudo binary not found. Please make sure that you are running the installer on a supported system.</source>
-        <translation type="unfinished"></translation>
+        <translation>No se encuentra el binario sudo. Por favor, asegúrese de que está iniciando el instalador en un sistema compatible.</translation>
     </message>
     <message>
         <source>There was an error while determining whether the language has already been set on this system.</source>
-        <translation type="unfinished"></translation>
+        <translation>Hubo un error intentando determinar el idioma que está seleccionado en el sistema.</translation>
     </message>
     <message>
         <source>Select Country</source>
-        <translation type="unfinished"></translation>
+        <translation>Seleccionar País</translation>
     </message>
     <message>
         <source>To set up the locale and keyboard layout, select a country.</source>
-        <translation type="unfinished"></translation>
+        <translation>Para seleccionar su ubicación y método de entrada del teclado, seleccione un país.</translation>
     </message>
     <message>
         <source>To set up the installation of FreeBSD, click &apos;Continue&apos;.</source>
-        <translation type="unfinished"></translation>
+        <translation>Para iniciar la instalación de FreeBSD, haga click en continuar.</translation>
     </message>
     <message>
         <source>FreeBSD is an operating system for a variety of platforms which focuses on features, speed, and stability. It is derived from BSD, the version of UNIXÂ® developed at the University of California, Berkeley. It is developed and maintained by a large community.</source>
-        <translation type="unfinished"></translation>
+        <translation>FreeBSD es un sistema operativo para variedad de plataformas, enfocado en la productividad, velocidad y estabilidad. Es derivado de BSD, la versión de UNIX desarrollada por la Universidad de California, Bekerley. Está desarrollado y mantenido por una gran comunidad.</translation>
     </message>
     <message>
         <source>License Terms</source>
-        <translation type="unfinished"></translation>
+        <translation>Acuerdo de Licencia</translation>
     </message>
     <message>
         <source>Additional components may be distributed under different licenses as stated in the respective documentation.</source>
-        <translation type="unfinished"></translation>
+        <translation>Los componentes adicionales pueden estar distribuidos bajo una licencia diferente a la que está definida en el respectivo documento.</translation>
     </message>
     <message>
         <source>Select Destination Disk</source>
-        <translation type="unfinished"></translation>
+        <translation>Seleccione el Disco de Destino</translation>
     </message>
     <message>
         <source>All data on the selected disk will be erased.</source>
-        <translation type="unfinished"></translation>
+        <translation>Todos los datos del disco seleccionado serán eliminados.</translation>
     </message>
     <message>
         <source>The installer script did not report the required disk space. Are you running the installer on a supported system? Can you run the installer script with sudo without needing a password?</source>
-        <translation type="unfinished"></translation>
+        <translation>El script de instalación no reportó el espacio requerido. ¿Está seguro de que está iniciándolo en un sistema compatible? ¿Puede iniciar el script con &quot;sudo&quot; sin necesidad de contraseña?</translation>
     </message>
     <message>
         <source>Disk space required: %s MiB</source>
-        <translation type="unfinished"></translation>
+        <translation>Espacio de disco requerido: %s MiB</translation>
     </message>
     <message>
         <source>Warning</source>
-        <translation type="unfinished"></translation>
+        <translation>Advertencia</translation>
     </message>
     <message>
         <source>This will erase all contents of this disk and install the FreeBSD operating system on it. Continue?</source>
-        <translation type="unfinished"></translation>
+        <translation>Esto eliminará todo el contenido del disco e instalará el sistema operativo FreeBSD en él. ¿Continuar?</translation>
     </message>
     <message>
         <source>Set Root Password</source>
-        <translation type="unfinished"></translation>
+        <translation>Seleccione contraseña de Administrador</translation>
     </message>
     <message>
         <source>Please enter a password for the root user.</source>
-        <translation type="unfinished"></translation>
+        <translation>Por favor, seleccione una contraseña para el usuario Administrador.</translation>
     </message>
     <message>
         <source>Password:</source>
-        <translation type="unfinished"></translation>
+        <translation>Contraseña:</translation>
     </message>
     <message>
         <source>Retype Password:</source>
-        <translation type="unfinished"></translation>
+        <translation>Repita la contraseña:</translation>
     </message>
     <message>
         <source>The passwords do not match</source>
-        <translation type="unfinished"></translation>
+        <translation>Las contraseñas no coinciden</translation>
     </message>
     <message>
         <source>Create Local Computer Account</source>
-        <translation type="unfinished"></translation>
+        <translation>Crear una cuenta Local en el equipo</translation>
     </message>
     <message>
         <source>Please enter details to create an account for the main user on this computer.</source>
-        <translation type="unfinished"></translation>
+        <translation>Por favor, introduzca los detalles para crear una cuenta para el usuario principal en este equipo.</translation>
     </message>
     <message>
         <source>Full Name:</source>
-        <translation type="unfinished"></translation>
+        <translation>Nombre Completo:</translation>
     </message>
     <message>
         <source>John Doe</source>
-        <translation type="unfinished"></translation>
+        <translation>John Doe</translation>
     </message>
     <message>
         <source>Username:</source>
-        <translation type="unfinished"></translation>
+        <translation>Nombre de usuario:</translation>
     </message>
     <message>
         <source>Retype Password</source>
-        <translation type="unfinished"></translation>
+        <translation>Repita la contraseña</translation>
     </message>
     <message>
         <source>Computer Name:</source>
-        <translation type="unfinished"></translation>
+        <translation>Nombre del Equipo:</translation>
     </message>
     <message>
         <source>Enable users to log in over the network (ssh)</source>
-        <translation type="unfinished"></translation>
+        <translation>Habilitar el inicio de sesión a través de la red (ssh)</translation>
     </message>
     <message>
         <source>Set time zone based on current location</source>
-        <translation type="unfinished"></translation>
+        <translation>Seleccione Fecha y Hora basado en su ubicación</translation>
     </message>
     <message>
         <source>Installing FreeBSD</source>
-        <translation type="unfinished"></translation>
+        <translation>Instalando FreeBSD</translation>
     </message>
     <message>
         <source>FreeBSD is being installed to your computer.</source>
-        <translation type="unfinished"></translation>
+        <translation>FreeBSD ha sido instalado en su equipo.</translation>
     </message>
     <message>
         <source>The selected disk device %s is not found.</source>
-        <translation type="unfinished"></translation>
+        <translation>El disco seleccionado (%s) no se encuentra.</translation>
     </message>
     <message>
         <source>Did not get permission to erase the target device.</source>
-        <translation type="unfinished"></translation>
+        <translation>No tiene los permisos para formatear este dispositivo.</translation>
     </message>
     <message>
         <source>The installer cannot be launched.</source>
-        <translation type="unfinished"></translation>
+        <translation>El instalador no puede ser iniciado.</translation>
     </message>
     <message>
         <source>The installation did not succeed. Please see the Installer Log for more information.</source>
-        <translation type="unfinished"></translation>
+        <translation>La instalación no pudo ser efectuada. Por favor, lea el Registro de Instalación para más información.</translation>
     </message>
     <message>
         <source>Installation Complete</source>
-        <translation type="unfinished"></translation>
+        <translation>Instalación Completada</translation>
     </message>
     <message>
         <source>The installation succeeded.</source>
-        <translation type="unfinished"></translation>
+        <translation>La instalación ha sido completada.</translation>
     </message>
     <message>
         <source>FreeBSD has been installed on your computer, click &apos;Restart&apos; to begin using it.</source>
-        <translation type="unfinished"></translation>
+        <translation>FreeBSD ha sido instalado en su sistema, haga click en &apos;Reiniciar&apos; para comenzar a usarlo.</translation>
     </message>
     <message>
         <source>Restart</source>
-        <translation type="unfinished"></translation>
+        <translation>Reiniciar</translation>
     </message>
     <message>
         <source>Could not restart the computer.</source>
-        <translation type="unfinished"></translation>
+        <translation>No se pudo reiniciar el equipo.</translation>
     </message>
     <message>
         <source>Error</source>
-        <translation type="unfinished"></translation>
+        <translation>Error</translation>
     </message>
     <message>
         <source>The installation could not be performed.</source>
-        <translation type="unfinished"></translation>
+        <translation>La instalación no pudo completarse.</translation>
     </message>
     <message>
         <source>Quit</source>
-        <translation type="unfinished"></translation>
+        <translation>Quitar</translation>
     </message>
 </context>
 <context>
     <name>tstr</name>
     <message>
         <source>Hello World</source>
-        <translation type="unfinished"></translation>
+        <translation>Hola Mundo</translation>
     </message>
 </context>
 </TS>
