0aa3c54be896c45e9a30bb7a2dedd4db72bcb8f4
vitorhcl <vitorhcl00@gmail.com>
2021-11-16 15:19:51 +0000

    Translated using Weblate (Portuguese (Brazil))
    Currently translated at 20.0% (4 of 20 strings)
    
    Translation: helloSystem/Hardware Probe
    Translate-URL: https://hosted.weblate.org/projects/hellosystem/hardware-probe/pt_BR/

--- Utilities/Hardware Probe.app/Resources/i18n/pt_BR.ts
+++ Utilities/Hardware Probe.app/Resources/i18n/pt_BR.ts
@@ -5,7 +5,7 @@
     <name>@default</name>
     <message>
         <source>An unknown error occured.</source>
-        <translation type="unfinished"></translation>
+        <translation>Ocorreu um erro desconhecido.</translation>
     </message>
     <message>
         <source>Hardware Probe</source>
@@ -67,7 +67,7 @@
     </message>
     <message>
         <source>Quit</source>
-        <translation type="unfinished"></translation>
+        <translation>Sair</translation>
     </message>
     <message>
         <source>Failed to open the uploaded hardware probe.</source>
@@ -75,7 +75,7 @@
     </message>
     <message>
         <source>Error</source>
-        <translation type="unfinished"></translation>
+        <translation>Erro</translation>
     </message>
     <message>
         <source>Hardware Probe was not successful.</source>
@@ -86,7 +86,7 @@
     <name>tstr</name>
     <message>
         <source>Hello World</source>
-        <translation type="unfinished"></translation>
+        <translation>Olá, Mundo</translation>
     </message>
 </context>
 </TS>
