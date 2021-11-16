39636c650a26adcd1034f68074ecbe0a8f899907
vitorhcl <vitorhcl00@gmail.com>
2021-11-16 19:41:31 +0000

    Translated using Weblate (Portuguese (Brazil))
    Currently translated at 60.0% (12 of 20 strings)
    
    Translation: helloSystem/Hardware Probe
    Translate-URL: https://hosted.weblate.org/projects/hellosystem/hardware-probe/pt_BR/

--- Utilities/Hardware Probe.app/Resources/i18n/pt_BR.ts
+++ Utilities/Hardware Probe.app/Resources/i18n/pt_BR.ts
@@ -9,37 +9,39 @@
     </message>
     <message>
         <source>Hardware Probe</source>
-        <translation type="unfinished"></translation>
+        <translation>Teste de Hardware</translation>
     </message>
     <message>
         <source>Privacy Information</source>
-        <translation type="unfinished"></translation>
+        <translation>Informação de Privacidade</translation>
     </message>
     <message>
         <source>Uploading a Hardware Probe is subject to the following Pricacy Terms.</source>
-        <translation type="unfinished"></translation>
+        <translation>O upload de um Teste de Hardware está sujeito aos seguintes Termos de Privacidade.</translation>
     </message>
     <message>
         <source>Please see %s for more information.</source>
-        <translation type="unfinished"></translation>
+        <translation>Por favor, consulte %s para mais informações.</translation>
     </message>
     <message>
         <source>&lt;p&gt;This utility collects hardware details of your computer and can anonymously upload them to a public database.&lt;/p&gt;
         &lt;p&gt;This can help users and operating system developers to collaboratively debug hardware related issues, check for operating system compatibility and find drivers.&lt;/p&gt;
         &lt;p&gt;You will get a permanent probe URL to view and share collected information.&lt;/p&gt;&lt;br&gt;&lt;br&gt;&lt;br&gt;</source>
-        <translation type="unfinished"></translation>
+        <translation>&lt;p&gt;Esse utilitário coleta detalhes do hardware do seu computador e um upload anônimo deles pode ser feito para um banco de dados público.&lt;/p&gt;
+        &lt;p&gt;Isso pode ajudar usuários e desenvolvedores de sistemas operacionais a depurar colaborativamente problemas relacionados ao hardware, verificar a compatibilidade do sistema operacional e encontrar drivers.&lt;/p&gt;
+        &lt;p&gt;Você vai obter uma URL permanente do teste para visualizar e compartilhar informações coletadas.&lt;/p&gt;&lt;br&gt;&lt;br&gt;&lt;br&gt;</translation>
     </message>
     <message>
         <source>Show Hardware Probe</source>
-        <translation type="unfinished"></translation>
+        <translation>Mostrar Teste de Hardware</translation>
     </message>
     <message>
         <source>Failed to open the hardware probe.</source>
-        <translation type="unfinished"></translation>
+        <translation>Falha ao abrir o teste de hardware.</translation>
     </message>
     <message>
         <source>Failed to run the %s tool.</source>
-        <translation type="unfinished"></translation>
+        <translation>Falha ao executar a ferramenta %s.</translation>
     </message>
     <message>
         <source>Failed to set the owner to %x.</source>
