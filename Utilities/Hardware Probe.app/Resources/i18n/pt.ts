9dccb2f9de62ca8fd8cc02f3a11fd68870ad7110
Hugo Carvalho <hugokarvalho@hotmail.com>
2021-12-04 23:01:28 +0000

    Translated using Weblate (Portuguese)
    Currently translated at 100.0% (20 of 20 strings)
    
    Translation: helloSystem/Hardware Probe
    Translate-URL: https://hosted.weblate.org/projects/hellosystem/hardware-probe/pt/

--- Utilities/Hardware Probe.app/Resources/i18n/pt.ts
+++ Utilities/Hardware Probe.app/Resources/i18n/pt.ts
@@ -9,61 +9,63 @@
     </message>
     <message>
         <source>Hardware Probe</source>
-        <translation type="unfinished"></translation>
+        <translation>Teste de Hardware</translation>
     </message>
     <message>
         <source>Privacy Information</source>
-        <translation type="unfinished"></translation>
+        <translation>Informação de Privacidade</translation>
     </message>
     <message>
         <source>Uploading a Hardware Probe is subject to the following Pricacy Terms.</source>
-        <translation type="unfinished"></translation>
+        <translation>O envio de um Teste de Hardware está sujeito aos seguintes Termos de Privacidade.</translation>
     </message>
     <message>
         <source>Please see %s for more information.</source>
-        <translation type="unfinished"></translation>
+        <translation>Consulte %s para mais informações.</translation>
     </message>
     <message>
         <source>&lt;p&gt;This utility collects hardware details of your computer and can anonymously upload them to a public database.&lt;/p&gt;
         &lt;p&gt;This can help users and operating system developers to collaboratively debug hardware related issues, check for operating system compatibility and find drivers.&lt;/p&gt;
         &lt;p&gt;You will get a permanent probe URL to view and share collected information.&lt;/p&gt;&lt;br&gt;&lt;br&gt;&lt;br&gt;</source>
-        <translation type="unfinished"></translation>
+        <translation>&lt;p&gt;Este utilitário recolhe detalhes do hardware do seu computador e o seu envio anónimo pode ser feito para uma base de dados pública.&lt;/p&gt;
+        &lt;p&gt;Isto pode ajudar utilizadores e programadores de sistemas operativos a depurar em colaboração problemas relacionados ao hardware, verificar a compatibilidade do sistema operativo e encontrar drivers.&lt;/p&gt;
+        &lt;p&gt;Irá obter uma URL permanente do teste para visualizar e partilhar informações recolhidas.&lt;/p&gt;&lt;br&gt;&lt;br&gt;&lt;br&gt;</translation>
     </message>
     <message>
         <source>Show Hardware Probe</source>
-        <translation type="unfinished"></translation>
+        <translation>Mostrar Teste de Hardware</translation>
     </message>
     <message>
         <source>Failed to open the hardware probe.</source>
-        <translation type="unfinished"></translation>
+        <translation>Falha ao abrir o teste de hardware.</translation>
     </message>
     <message>
         <source>Failed to run the %s tool.</source>
-        <translation type="unfinished"></translation>
+        <translation>Falha ao executar a ferramenta %s.</translation>
     </message>
     <message>
         <source>Failed to set the owner to %x.</source>
-        <translation type="unfinished"></translation>
+        <translation>Falha ao definir o dono de %x.</translation>
     </message>
     <message>
         <source>Uploading Hardware Probe</source>
-        <translation type="unfinished"></translation>
+        <translation>A enviar o Teste de Hardware</translation>
     </message>
     <message>
         <source>The Hardware Probe is being uploaded to the public database</source>
-        <translation type="unfinished"></translation>
+        <translation>O Teste de Hardware está a ser enviado para a base de dados pública</translation>
     </message>
     <message>
         <source>You need an active internet connection in order to upload.</source>
-        <translation type="unfinished"></translation>
+        <translation>É necessária uma ligação ativa à Internet afim de enviar.</translation>
     </message>
     <message>
         <source>Failed to upload using the %s tool.</source>
-        <translation type="unfinished"></translation>
+        <translation>Falha no envio ao usar a ferramenta %s.</translation>
     </message>
     <message>
         <source>Hardware Probe Uploaded</source>
-        <translation type="unfinished"></translation>
+        <translation>Teste de Hardware enviado</translation>
     </message>
     <message>
         <source>Quit</source>
@@ -71,7 +73,7 @@
     </message>
     <message>
         <source>Failed to open the uploaded hardware probe.</source>
-        <translation type="unfinished"></translation>
+        <translation>Não foi possível enviar o teste de hardware.</translation>
     </message>
     <message>
         <source>Error</source>
@@ -79,14 +81,14 @@
     </message>
     <message>
         <source>Hardware Probe was not successful.</source>
-        <translation type="unfinished"></translation>
+        <translation>O Teste de Hardware não foi bem sucedido.</translation>
     </message>
 </context>
 <context>
     <name>tstr</name>
     <message>
         <source>Hello World</source>
-        <translation>Olá, Mundo</translation>
+        <translation>Olá Mundo</translation>
     </message>
 </context>
 </TS>
