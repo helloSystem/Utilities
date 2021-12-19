85078428921ddced67e0109f43e46c7760ffd503
Cseszneki Máté <cseszneki.mate.69@gmail.com>
2021-12-19 18:22:49 +0000

    Translated using Weblate (Hungarian)
    Currently translated at 100.0% (51 of 51 strings)
    
    Translation: helloSystem/Install FreeBSD
    Translate-URL: https://hosted.weblate.org/projects/hellosystem/install/hu/

--- Utilities/Install FreeBSD.app/Resources/i18n/hu.ts
+++ Utilities/Install FreeBSD.app/Resources/i18n/hu.ts
@@ -5,210 +5,210 @@
     <name>@default</name>
     <message>
         <source>Install FreeBSD</source>
-        <translation type="unfinished"></translation>
+        <translation>FreeBSD Telepítése</translation>
     </message>
     <message>
         <source>An unknown error occured.</source>
-        <translation type="unfinished"></translation>
+        <translation>Ismeretlen hiba történt.</translation>
     </message>
     <message>
         <source>Pre-existing %s removed</source>
-        <translation type="unfinished"></translation>
+        <translation>A létező %s eltávolítva</translation>
     </message>
     <message>
         <source>Installer Log</source>
-        <translation type="unfinished"></translation>
+        <translation>Telepítési Napló</translation>
     </message>
     <message>
         <source>The Installer Log cannot be opened.</source>
-        <translation type="unfinished"></translation>
+        <translation>A Telepítési Naplót nem lehet megnyitni.</translation>
     </message>
     <message>
         <source>Question</source>
-        <translation type="unfinished"></translation>
+        <translation>Kérdés</translation>
     </message>
     <message>
         <source>This will send a request to ipapi.com one time to locate your computer over the network. Continue?</source>
-        <translation type="unfinished"></translation>
+        <translation>Egyszeri kérést fog küldeni a ipapi.com-ra , hogy megtalálja a számítógépedet a hálózaton. Folytatod?</translation>
     </message>
     <message>
         <source>The installer script was not found. Please make sure that you are running the installer on a supported system.</source>
-        <translation type="unfinished"></translation>
+        <translation>A telepítő szkript nem található. Bizonyosodj meg hogy a telepítőt egy támogatott rendszeren futtatod.</translation>
     </message>
     <message>
         <source>The sudo binary not found. Please make sure that you are running the installer on a supported system.</source>
-        <translation type="unfinished"></translation>
+        <translation>A sudo bináris nem található. Bizonyosodj meg hogy a telepítőt egy támogatott rendszeren futtatod.</translation>
     </message>
     <message>
         <source>There was an error while determining whether the language has already been set on this system.</source>
-        <translation type="unfinished"></translation>
+        <translation>Hiba történt annak meghatározása során, hogy a nyelv már be van-e állítva ezen a rendszeren.</translation>
     </message>
     <message>
         <source>Select Country</source>
-        <translation type="unfinished"></translation>
+        <translation>Ország kiválasztása</translation>
     </message>
     <message>
         <source>To set up the locale and keyboard layout, select a country.</source>
-        <translation type="unfinished"></translation>
+        <translation>Válassz egy országot, hogy beállíthassuk a nyelvet és a billentyűzetkiosztást.</translation>
     </message>
     <message>
         <source>To set up the installation of FreeBSD, click &apos;Continue&apos;.</source>
-        <translation type="unfinished"></translation>
+        <translation>A FreeBSD telepítésének beállításához, kattints a &apos;Folytatás&apos;-ra.</translation>
     </message>
     <message>
         <source>FreeBSD is an operating system for a variety of platforms which focuses on features, speed, and stability. It is derived from BSD, the version of UNIXÂ® developed at the University of California, Berkeley. It is developed and maintained by a large community.</source>
-        <translation type="unfinished"></translation>
+        <translation>A FreeBSD egy operációs rendszer számos platformhoz, amely a funkcionalitásra, a sebességre és a stabilitásra összpontosít. A BSD leszármazottja, amely egy verziója az UNIX®-nak amelyet a Berkeley-ben működő Kaliforniai Egyetemen fejlesztettek. Fejlesztését és fenntartását egy nagy közösség végzi.</translation>
     </message>
     <message>
         <source>License Terms</source>
-        <translation type="unfinished"></translation>
+        <translation>Licensz Feltételek</translation>
     </message>
     <message>
         <source>Additional components may be distributed under different licenses as stated in the respective documentation.</source>
-        <translation type="unfinished"></translation>
+        <translation>További összetevők terjeszthetők különböző licencek alatt, amint az a megfelelő dokumentációban szerepel.</translation>
     </message>
     <message>
         <source>Select Destination Disk</source>
-        <translation type="unfinished"></translation>
+        <translation>Válaszd Ki A Céllemezt</translation>
     </message>
     <message>
         <source>All data on the selected disk will be erased.</source>
-        <translation type="unfinished"></translation>
+        <translation>Minden adat törlésre kerül a kiválasztott lemezen.</translation>
     </message>
     <message>
         <source>The installer script did not report the required disk space. Are you running the installer on a supported system? Can you run the installer script with sudo without needing a password?</source>
-        <translation type="unfinished"></translation>
+        <translation>A telepítő szkript nem találja a szükséges szabad helyet a lemezen. Biztos hogy a telepítőt egy támogatott rendszeren futtatod? Tudod futtatni a telepítő szkriptet sudo-val jelszó nélkül?</translation>
     </message>
     <message>
         <source>Disk space required: %s MiB</source>
-        <translation type="unfinished"></translation>
+        <translation>A lemezen ennyi helyre van szükség: %s MiB</translation>
     </message>
     <message>
         <source>Warning</source>
-        <translation type="unfinished"></translation>
+        <translation>Figyelem</translation>
     </message>
     <message>
         <source>This will erase all contents of this disk and install the FreeBSD operating system on it. Continue?</source>
-        <translation type="unfinished"></translation>
+        <translation>Minden adatot töröl a lemezről és telepíti a FreeBSD operációs rendszert. Folytatod?</translation>
     </message>
     <message>
         <source>Set Root Password</source>
-        <translation type="unfinished"></translation>
+        <translation>Rendszergazdai fiók jelszavának beállítása</translation>
     </message>
     <message>
         <source>Please enter a password for the root user.</source>
-        <translation type="unfinished"></translation>
+        <translation>Kérlek adj meg egy jelszót a rendszergazdai fiókhoz.</translation>
     </message>
     <message>
         <source>Password:</source>
-        <translation type="unfinished"></translation>
+        <translation>Jelszó:</translation>
     </message>
     <message>
         <source>Retype Password:</source>
-        <translation type="unfinished"></translation>
+        <translation>Jelszó újra:</translation>
     </message>
     <message>
         <source>The passwords do not match</source>
-        <translation type="unfinished"></translation>
+        <translation>A jelszavak nem egyeznek</translation>
     </message>
     <message>
         <source>Create Local Computer Account</source>
-        <translation type="unfinished"></translation>
+        <translation>Helyi fiók létrehozása</translation>
     </message>
     <message>
         <source>Please enter details to create an account for the main user on this computer.</source>
-        <translation type="unfinished"></translation>
+        <translation>Kérlek add meg a fő felhasználói adatokat, hogy létrehozhassunk egy fiókot a számítógépen.</translation>
     </message>
     <message>
         <source>Full Name:</source>
-        <translation type="unfinished"></translation>
+        <translation>Teljes név:</translation>
     </message>
     <message>
         <source>John Doe</source>
-        <translation type="unfinished"></translation>
+        <translation>Kovács István</translation>
     </message>
     <message>
         <source>Username:</source>
-        <translation type="unfinished"></translation>
+        <translation>Felhasználónév:</translation>
     </message>
     <message>
         <source>Retype Password</source>
-        <translation type="unfinished"></translation>
+        <translation>Jelszó újra</translation>
     </message>
     <message>
         <source>Computer Name:</source>
-        <translation type="unfinished"></translation>
+        <translation>Számítógép neve:</translation>
     </message>
     <message>
         <source>Enable users to log in over the network (ssh)</source>
-        <translation type="unfinished"></translation>
+        <translation>Engedélyezni a felhasználók hálózaton keresztüli bejelentkezését (ssh)</translation>
     </message>
     <message>
         <source>Set time zone based on current location</source>
-        <translation type="unfinished"></translation>
+        <translation>Időzona beállítása a jelenlegi tartozkodási hely segítségével</translation>
     </message>
     <message>
         <source>Installing FreeBSD</source>
-        <translation type="unfinished"></translation>
+        <translation>A FreeBSD telepítése</translation>
     </message>
     <message>
         <source>FreeBSD is being installed to your computer.</source>
-        <translation type="unfinished"></translation>
+        <translation>FreeBSD jelenleg a számítógépre települ.</translation>
     </message>
     <message>
         <source>The selected disk device %s is not found.</source>
-        <translation type="unfinished"></translation>
+        <translation>A kiválasztott lemez %s nem található.</translation>
     </message>
     <message>
         <source>Did not get permission to erase the target device.</source>
-        <translation type="unfinished"></translation>
+        <translation>Nem adtál jogosultságot a céllemez törlésére.</translation>
     </message>
     <message>
         <source>The installer cannot be launched.</source>
-        <translation type="unfinished"></translation>
+        <translation>A telepítő nem indítható.</translation>
     </message>
     <message>
         <source>The installation did not succeed. Please see the Installer Log for more information.</source>
-        <translation type="unfinished"></translation>
+        <translation>A telepítés sikertelen volt. Kérlek nézd meg a Telepítési Naplót több információért.</translation>
     </message>
     <message>
         <source>Installation Complete</source>
-        <translation type="unfinished"></translation>
+        <translation>A telepítés befejeződött</translation>
     </message>
     <message>
         <source>The installation succeeded.</source>
-        <translation type="unfinished"></translation>
+        <translation>A telepítés sikeres.</translation>
     </message>
     <message>
         <source>FreeBSD has been installed on your computer, click &apos;Restart&apos; to begin using it.</source>
-        <translation type="unfinished"></translation>
+        <translation>A FreeBSD telepítése sikeres volt, kattints az &apos;Újraindítás&apos;-ra hogy használatba vehesd.</translation>
     </message>
     <message>
         <source>Restart</source>
-        <translation type="unfinished"></translation>
+        <translation>Újraindítás</translation>
     </message>
     <message>
         <source>Could not restart the computer.</source>
-        <translation type="unfinished"></translation>
+        <translation>Nem sikerült újraindítani a számítógépet.</translation>
     </message>
     <message>
         <source>Error</source>
-        <translation type="unfinished"></translation>
+        <translation>Hiba</translation>
     </message>
     <message>
         <source>The installation could not be performed.</source>
-        <translation type="unfinished"></translation>
+        <translation>A telepítést nem sikerült végrehajtani.</translation>
     </message>
     <message>
         <source>Quit</source>
-        <translation type="unfinished"></translation>
+        <translation>Kilépés</translation>
     </message>
 </context>
 <context>
     <name>tstr</name>
     <message>
         <source>Hello World</source>
-        <translation type="unfinished"></translation>
+        <translation>Hello Világ</translation>
     </message>
 </context>
 </TS>
