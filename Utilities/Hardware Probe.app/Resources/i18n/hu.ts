1ebd6f95ce710d17d204c12d20cc6553cc2556cd
Balázs Máhli <mahli@posteo.net>
2021-12-27 22:53:38 +0000

    Translated using Weblate (Hungarian)
    Currently translated at 100.0% (20 of 20 strings)
    
    Translation: helloSystem/Hardware Probe
    Translate-URL: https://hosted.weblate.org/projects/hellosystem/hardware-probe/hu/

--- Utilities/Hardware Probe.app/Resources/i18n/hu.ts
+++ Utilities/Hardware Probe.app/Resources/i18n/hu.ts
@@ -9,7 +9,7 @@
     </message>
     <message>
         <source>Hardware Probe</source>
-        <translation>Hardver szkennelés</translation>
+        <translation>Hardverinformáció</translation>
     </message>
     <message>
         <source>Privacy Information</source>
@@ -17,7 +17,7 @@
     </message>
     <message>
         <source>Uploading a Hardware Probe is subject to the following Pricacy Terms.</source>
-        <translation type="unfinished"></translation>
+        <translation>A hardverinformációval feltöltött adatokra az Adatvédelmi Irányelvek vonatkoznak.</translation>
     </message>
     <message>
         <source>Please see %s for more information.</source>
@@ -27,43 +27,45 @@
         <source>&lt;p&gt;This utility collects hardware details of your computer and can anonymously upload them to a public database.&lt;/p&gt;
         &lt;p&gt;This can help users and operating system developers to collaboratively debug hardware related issues, check for operating system compatibility and find drivers.&lt;/p&gt;
         &lt;p&gt;You will get a permanent probe URL to view and share collected information.&lt;/p&gt;&lt;br&gt;&lt;br&gt;&lt;br&gt;</source>
-        <translation type="unfinished"></translation>
+        <translation>&lt;p&gt;Ez az eszköz anonim módon, névtelenül adatokat gyűjt a számítógépéről, amit egy nyílvános adatbázisba tölt fel&lt;/p&gt;
+        &lt;p&gt;Ez segíthet más felhasználóknak, hogy közösen tárjanak fel hardverrel kapcsolatos hibákat és ellenőrizzek az operációs rendszer kompatibilitását.&lt;/p&gt;
+        &lt;p&gt;Ön egy örökös webcímet fog kapni, hogy megtekinthesse és megoszthassa a gyűjtött adatokat.&lt;/p&gt;&lt;br&gt;&lt;br&gt;&lt;br&gt;</translation>
     </message>
     <message>
         <source>Show Hardware Probe</source>
-        <translation type="unfinished"></translation>
+        <translation>Hardver információk mutatása</translation>
     </message>
     <message>
         <source>Failed to open the hardware probe.</source>
-        <translation type="unfinished"></translation>
+        <translation>Nem sikerült a hardverinformációk megnyitása.</translation>
     </message>
     <message>
         <source>Failed to run the %s tool.</source>
-        <translation type="unfinished"></translation>
+        <translation>Nem sikerült a %s eszköz futtatása.</translation>
     </message>
     <message>
         <source>Failed to set the owner to %x.</source>
-        <translation type="unfinished"></translation>
+        <translation>Nem sikerült a tulajdonos %x-re állítása.</translation>
     </message>
     <message>
         <source>Uploading Hardware Probe</source>
-        <translation type="unfinished"></translation>
+        <translation>Hardverinformáció feltöltése</translation>
     </message>
     <message>
         <source>The Hardware Probe is being uploaded to the public database</source>
-        <translation type="unfinished"></translation>
+        <translation>A hardverinformációk feltöltődnek az adatbázisba</translation>
     </message>
     <message>
         <source>You need an active internet connection in order to upload.</source>
-        <translation type="unfinished"></translation>
+        <translation>A feltöltéshez internetkapcsolatra van szüksége.</translation>
     </message>
     <message>
         <source>Failed to upload using the %s tool.</source>
-        <translation type="unfinished"></translation>
+        <translation>Sikertelen feltöltés a(z) %s eszközzel.</translation>
     </message>
     <message>
         <source>Hardware Probe Uploaded</source>
-        <translation type="unfinished"></translation>
+        <translation>Hardverinformáció feltöltve</translation>
     </message>
     <message>
         <source>Quit</source>
@@ -71,7 +73,7 @@
     </message>
     <message>
         <source>Failed to open the uploaded hardware probe.</source>
-        <translation type="unfinished"></translation>
+        <translation>Nem sikerült a feltöltött hardverinformáció megnyitása.</translation>
     </message>
     <message>
         <source>Error</source>
@@ -79,7 +81,7 @@
     </message>
     <message>
         <source>Hardware Probe was not successful.</source>
-        <translation type="unfinished"></translation>
+        <translation>A hardverinformáció gyűjtés nem sikerült.</translation>
     </message>
 </context>
 <context>
