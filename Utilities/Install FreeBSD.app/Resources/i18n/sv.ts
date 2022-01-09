ca8675acf998ec2a5cdbc3a854abefee5c2a5979
Luna Jernberg <droidbittin@gmail.com>
2022-01-09 13:16:22 +0000

    Translated using Weblate (Swedish)
    Currently translated at 100.0% (51 of 51 strings)
    
    Translation: helloSystem/Install FreeBSD
    Translate-URL: https://hosted.weblate.org/projects/hellosystem/install/sv/

--- Utilities/Install FreeBSD.app/Resources/i18n/sv.ts
+++ Utilities/Install FreeBSD.app/Resources/i18n/sv.ts
@@ -5,67 +5,67 @@
     <name>@default</name>
     <message>
         <source>Install FreeBSD</source>
-        <translation type="unfinished"></translation>
+        <translation>Installera FreeBSD</translation>
     </message>
     <message>
         <source>An unknown error occured.</source>
-        <translation type="unfinished"></translation>
+        <translation>Ett okänt fel uppstod.</translation>
     </message>
     <message>
         <source>Pre-existing %s removed</source>
-        <translation type="unfinished"></translation>
+        <translation>Redan existerande %s har tagits bort</translation>
     </message>
     <message>
         <source>Installer Log</source>
-        <translation type="unfinished"></translation>
+        <translation>Installatörslogg</translation>
     </message>
     <message>
         <source>The Installer Log cannot be opened.</source>
-        <translation type="unfinished"></translation>
+        <translation>Installatörsloggen kan inte öppnas.</translation>
     </message>
     <message>
         <source>Question</source>
-        <translation type="unfinished"></translation>
+        <translation>Fråga</translation>
     </message>
     <message>
         <source>This will send a request to ipapi.com one time to locate your computer over the network. Continue?</source>
-        <translation type="unfinished"></translation>
+        <translation>Detta kommer att skicka en begäran till ipapi.com en gång för att hitta din dator över nätverket. Fortsätt?</translation>
     </message>
     <message>
         <source>The installer script was not found. Please make sure that you are running the installer on a supported system.</source>
-        <translation type="unfinished"></translation>
+        <translation>Installationsskriptet hittades inte. Se till att du kör installationsprogrammet på ett system som stöds.</translation>
     </message>
     <message>
         <source>The sudo binary not found. Please make sure that you are running the installer on a supported system.</source>
-        <translation type="unfinished"></translation>
+        <translation>Sudo-binären hittades inte. Se till att du kör installationsprogrammet på ett system som stöds.</translation>
     </message>
     <message>
         <source>There was an error while determining whether the language has already been set on this system.</source>
-        <translation type="unfinished"></translation>
+        <translation>Det uppstod ett fel när det skulle fastställas om språket redan har ställts in på det här systemet.</translation>
     </message>
     <message>
         <source>Select Country</source>
-        <translation type="unfinished"></translation>
+        <translation>Välj Land</translation>
     </message>
     <message>
         <source>To set up the locale and keyboard layout, select a country.</source>
-        <translation type="unfinished"></translation>
+        <translation>För att ställa in språk och tangentbordslayout, välj ett land.</translation>
     </message>
     <message>
         <source>To set up the installation of FreeBSD, click &apos;Continue&apos;.</source>
-        <translation type="unfinished"></translation>
+        <translation>För att ställa in installationen av FreeBSD, klicka på &apos;Fortsätt&apos;.</translation>
     </message>
     <message>
         <source>FreeBSD is an operating system for a variety of platforms which focuses on features, speed, and stability. It is derived from BSD, the version of UNIXÂ® developed at the University of California, Berkeley. It is developed and maintained by a large community.</source>
-        <translation type="unfinished"></translation>
+        <translation>FreeBSD är ett operativsystem för en mängd olika plattformar som fokuserar på funktioner, hastighet och stabilitet. Det härrör från BSD, versionen av UNIX® utvecklad vid University of California, Berkeley. Det utvecklas och underhålls av en stor gemenskap.</translation>
     </message>
     <message>
         <source>License Terms</source>
-        <translation type="unfinished"></translation>
+        <translation>Licensvillkor</translation>
     </message>
     <message>
         <source>Additional components may be distributed under different licenses as stated in the respective documentation.</source>
-        <translation type="unfinished"></translation>
+        <translation>Ytterligare komponenter kan distribueras under olika licenser som anges i respektive dokumentation.</translation>
     </message>
     <message>
         <source>Select Destination Disk</source>
@@ -77,7 +77,7 @@
     </message>
     <message>
         <source>The installer script did not report the required disk space. Are you running the installer on a supported system? Can you run the installer script with sudo without needing a password?</source>
-        <translation type="unfinished"></translation>
+        <translation>Installationsskriptet rapporterade inte det nödvändiga diskutrymmet. Kör du installationsprogrammet på ett system som stöds? Kan du köra installationsskriptet med sudo utan att behöva ett lösenord?</translation>
     </message>
     <message>
         <source>Disk space required: %s MiB</source>
@@ -89,107 +89,107 @@
     </message>
     <message>
         <source>This will erase all contents of this disk and install the FreeBSD operating system on it. Continue?</source>
-        <translation type="unfinished"></translation>
+        <translation>Detta kommer att radera allt innehåll på denna disk och installera FreeBSD-operativsystemet på den. Fortsätt?</translation>
     </message>
     <message>
         <source>Set Root Password</source>
-        <translation type="unfinished"></translation>
+        <translation>Ställ in rootlösenord</translation>
     </message>
     <message>
         <source>Please enter a password for the root user.</source>
-        <translation type="unfinished"></translation>
+        <translation>Ange ett lösenord för root-användaren.</translation>
     </message>
     <message>
         <source>Password:</source>
-        <translation type="unfinished"></translation>
+        <translation>Lösenord:</translation>
     </message>
     <message>
         <source>Retype Password:</source>
-        <translation type="unfinished"></translation>
+        <translation>Skriv lösenordet igen:</translation>
     </message>
     <message>
         <source>The passwords do not match</source>
-        <translation type="unfinished"></translation>
+        <translation>Lösenorden stämmer inte överens</translation>
     </message>
     <message>
         <source>Create Local Computer Account</source>
-        <translation type="unfinished"></translation>
+        <translation>Skapa ett lokalt datorkonto</translation>
     </message>
     <message>
         <source>Please enter details to create an account for the main user on this computer.</source>
-        <translation type="unfinished"></translation>
+        <translation>Ange detaljer för att skapa ett konto för huvudanvändaren på den här datorn.</translation>
     </message>
     <message>
         <source>Full Name:</source>
-        <translation type="unfinished"></translation>
+        <translation>Fullständigt namn:</translation>
     </message>
     <message>
         <source>John Doe</source>
-        <translation type="unfinished"></translation>
+        <translation>John Doe</translation>
     </message>
     <message>
         <source>Username:</source>
-        <translation type="unfinished"></translation>
+        <translation>Användarnamn:</translation>
     </message>
     <message>
         <source>Retype Password</source>
-        <translation type="unfinished"></translation>
+        <translation>Skriv lösenordet igen</translation>
     </message>
     <message>
         <source>Computer Name:</source>
-        <translation type="unfinished"></translation>
+        <translation>Datornamn:</translation>
     </message>
     <message>
         <source>Enable users to log in over the network (ssh)</source>
-        <translation type="unfinished"></translation>
+        <translation>Gör det möjligt för användare att logga in över nätverket (ssh)</translation>
     </message>
     <message>
         <source>Set time zone based on current location</source>
-        <translation type="unfinished"></translation>
+        <translation>Ställ in tidszon baserat på aktuell plats</translation>
     </message>
     <message>
         <source>Installing FreeBSD</source>
-        <translation type="unfinished"></translation>
+        <translation>Installerar FreeBSD</translation>
     </message>
     <message>
         <source>FreeBSD is being installed to your computer.</source>
-        <translation type="unfinished"></translation>
+        <translation>FreeBSD installeras på din dator.</translation>
     </message>
     <message>
         <source>The selected disk device %s is not found.</source>
-        <translation type="unfinished"></translation>
+        <translation>Den valda diskenheten %s hittades inte.</translation>
     </message>
     <message>
         <source>Did not get permission to erase the target device.</source>
-        <translation type="unfinished"></translation>
+        <translation>Fick inte tillstånd att radera målenheten.</translation>
     </message>
     <message>
         <source>The installer cannot be launched.</source>
-        <translation type="unfinished"></translation>
+        <translation>Installationsprogrammet kan inte startas.</translation>
     </message>
     <message>
         <source>The installation did not succeed. Please see the Installer Log for more information.</source>
-        <translation type="unfinished"></translation>
+        <translation>Installationen lyckades inte. Se installationsloggen för mer information.</translation>
     </message>
     <message>
         <source>Installation Complete</source>
-        <translation type="unfinished"></translation>
+        <translation>Installationen är klar</translation>
     </message>
     <message>
         <source>The installation succeeded.</source>
-        <translation type="unfinished"></translation>
+        <translation>Installationen lyckades.</translation>
     </message>
     <message>
         <source>FreeBSD has been installed on your computer, click &apos;Restart&apos; to begin using it.</source>
-        <translation type="unfinished"></translation>
+        <translation>FreeBSD har installerats på din dator, klicka på &quot;Starta om&quot; för att börja använda det.</translation>
     </message>
     <message>
         <source>Restart</source>
-        <translation type="unfinished"></translation>
+        <translation>Starta om</translation>
     </message>
     <message>
         <source>Could not restart the computer.</source>
-        <translation type="unfinished"></translation>
+        <translation>Kunde inte starta om datorn.</translation>
     </message>
     <message>
         <source>Error</source>
