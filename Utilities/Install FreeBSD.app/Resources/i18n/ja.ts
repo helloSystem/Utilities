b27e72256692dc3cc2caba04ebd9ca90298da7a1
タグコウ <blueglasses2008@gmail.com>
2023-02-19 12:33:31 +0000

    Translated using Weblate (Japanese)
    Currently translated at 100.0% (51 of 51 strings)
    
    Translation: helloSystem/Install FreeBSD
    Translate-URL: https://hosted.weblate.org/projects/hellosystem/install/ja/

--- Utilities/Install FreeBSD.app/Resources/i18n/ja.ts
+++ Utilities/Install FreeBSD.app/Resources/i18n/ja.ts
@@ -5,79 +5,79 @@
     <name>@default</name>
     <message>
         <source>Install FreeBSD</source>
-        <translation type="unfinished"></translation>
+        <translation>FreeBSDのインストール</translation>
     </message>
     <message>
         <source>An unknown error occurred.</source>
-        <translation>不明なエラーが発生しました</translation>
+        <translation>不明なエラーが発生しました。</translation>
     </message>
     <message>
         <source>Pre-existing %s removed</source>
-        <translation type="unfinished"></translation>
+        <translation>既存の %s を削除</translation>
     </message>
     <message>
         <source>Installer Log</source>
-        <translation type="unfinished"></translation>
+        <translation>インストラーのログ</translation>
     </message>
     <message>
         <source>The Installer Log cannot be opened.</source>
-        <translation type="unfinished"></translation>
+        <translation>Installer Logを開くことができません。</translation>
     </message>
     <message>
         <source>Question</source>
-        <translation type="unfinished"></translation>
+        <translation>質問</translation>
     </message>
     <message>
         <source>This will send a request to ipapi.com one time to locate your computer over the network. Continue?</source>
-        <translation type="unfinished"></translation>
+        <translation>これにより、ネットワーク上であなたのコンピュータを見つけるためのリクエストが、ipapi.comに一度だけ送信されます。続行しますか？</translation>
     </message>
     <message>
         <source>The installer script was not found. Please make sure that you are running the installer on a supported system.</source>
-        <translation type="unfinished"></translation>
+        <translation>インストーラースクリプトが見つかりませんでした。インストーラがサポートされているシステムで実行されていることを確認してください。</translation>
     </message>
     <message>
         <source>The sudo binary not found. Please make sure that you are running the installer on a supported system.</source>
-        <translation type="unfinished"></translation>
+        <translation>sudo バイナリが見つかりません。サポートされているシステムでインストーラを実行していることを確認してください。</translation>
     </message>
     <message>
         <source>There was an error while determining whether the language has already been set on this system.</source>
-        <translation type="unfinished"></translation>
+        <translation>このシステムで言語がすでに設定されているかどうかを判断する際にエラーが発生しました。</translation>
     </message>
     <message>
         <source>Select Country</source>
-        <translation type="unfinished"></translation>
+        <translation>国の選択</translation>
     </message>
     <message>
         <source>To set up the locale and keyboard layout, select a country.</source>
-        <translation type="unfinished"></translation>
+        <translation>ロケールやキーボードレイアウトを設定するには、国を選択します。</translation>
     </message>
     <message>
         <source>To set up the installation of FreeBSD, click &apos;Continue&apos;.</source>
-        <translation type="unfinished"></translation>
+        <translation>FreeBSD のインストールをセットアップするには、「続行」をクリックしてください。</translation>
     </message>
     <message>
         <source>FreeBSD is an operating system for a variety of platforms which focuses on features, speed, and stability. It is derived from BSD, the version of UNIXÂ® developed at the University of California, Berkeley. It is developed and maintained by a large community.</source>
-        <translation type="unfinished"></translation>
+        <translation>FreeBSD は、機能、速度、安定性に重点を置いた、さまざまなプラットフォーム向けのオペレーティングシステムです。カリフォルニア大学バークレー校で開発された UNIX® の BSD から派生したものです。大規模なコミュニティによって開発・保守されています。</translation>
     </message>
     <message>
         <source>License Terms</source>
-        <translation type="unfinished"></translation>
+        <translation>ライセンス条項</translation>
     </message>
     <message>
         <source>Additional components may be distributed under different licenses as stated in the respective documentation.</source>
-        <translation type="unfinished"></translation>
+        <translation>その他のコンポーネントは、それぞれのドキュメントに記載されているように、異なるライセンスの下で配布される場合があります。</translation>
     </message>
     <message>
         <source>Select Destination Disk</source>
-        <translation>宛先ディスクの選択</translation>
+        <translation>保存先ディスクの選択</translation>
     </message>
     <message>
         <source>All data on the selected disk will be erased.</source>
-        <translation type="unfinished"></translation>
+        <translation>選択したディスクのデータがすべて消去されます。</translation>
     </message>
     <message>
         <source>The installer script did not report the required disk space. Are you running the installer on a supported system? Can you run the installer script with sudo without needing a password?</source>
-        <translation type="unfinished"></translation>
+        <translation>インストーラー スクリプトは、必要なディスク領域を報告しませんでした。サポートされているシステムでインストーラーを実行していますか?パスワードを必要とせずにsudoでインストーラースクリプトを実行できますか?</translation>
     </message>
     <message>
         <source>Disk space required: %s MiB</source>
@@ -85,111 +85,111 @@
     </message>
     <message>
         <source>Warning</source>
-        <translation>注意</translation>
+        <translation>警告</translation>
     </message>
     <message>
         <source>This will erase all contents of this disk and install the FreeBSD operating system on it. Continue?</source>
-        <translation type="unfinished"></translation>
+        <translation>これにより、このディスクのすべての内容が消去され、FreeBSDオペレーティングシステムがインストールされます。続行しますか？</translation>
     </message>
     <message>
         <source>Set Root Password</source>
-        <translation type="unfinished"></translation>
+        <translation>Rootパスワードの設定</translation>
     </message>
     <message>
         <source>Please enter a password for the root user.</source>
-        <translation type="unfinished"></translation>
+        <translation>rootユーザーのパスワードを入力してください。</translation>
     </message>
     <message>
         <source>Password:</source>
-        <translation type="unfinished"></translation>
+        <translation>パスワード:</translation>
     </message>
     <message>
         <source>Retype Password:</source>
-        <translation type="unfinished"></translation>
+        <translation>パスワードを確認:</translation>
     </message>
     <message>
         <source>The passwords do not match</source>
-        <translation type="unfinished"></translation>
+        <translation>パスワードが一致しません</translation>
     </message>
     <message>
         <source>Create Local Computer Account</source>
-        <translation type="unfinished"></translation>
+        <translation>ローカルなコンピュータアカウントの作成</translation>
     </message>
     <message>
         <source>Please enter details to create an account for the main user on this computer.</source>
-        <translation type="unfinished"></translation>
+        <translation>このコンピュータのメインユーザーのアカウントを作成するため、詳細を入力してください。</translation>
     </message>
     <message>
         <source>Full Name:</source>
-        <translation type="unfinished"></translation>
+        <translation>フルネーム:</translation>
     </message>
     <message>
         <source>John Doe</source>
-        <translation type="unfinished"></translation>
+        <translation>ジョン・ドウ</translation>
     </message>
     <message>
         <source>Username:</source>
-        <translation type="unfinished"></translation>
+        <translation>ユーザー名:</translation>
     </message>
     <message>
         <source>Retype Password</source>
-        <translation type="unfinished"></translation>
+        <translation>パスワードの確認</translation>
     </message>
     <message>
         <source>Computer Name:</source>
-        <translation type="unfinished"></translation>
+        <translation>コンピューターの名前:</translation>
     </message>
     <message>
         <source>Enable users to log in over the network (ssh)</source>
-        <translation type="unfinished"></translation>
+        <translation>ユーザーがネットワーク経由でログインできるようにする（ssh）</translation>
     </message>
     <message>
         <source>Set time zone based on current location</source>
-        <translation type="unfinished"></translation>
+        <translation>現在地からタイムゾーンを設定する</translation>
     </message>
     <message>
         <source>Installing FreeBSD</source>
-        <translation type="unfinished"></translation>
+        <translation>FreeBSDをインストール中</translation>
     </message>
     <message>
         <source>FreeBSD is being installed to your computer.</source>
-        <translation type="unfinished"></translation>
+        <translation>FreeBSDがあなたのコンピュータにインストールされます。</translation>
     </message>
     <message>
         <source>The selected disk device %s is not found.</source>
-        <translation type="unfinished"></translation>
+        <translation>選択されたディスク・デバイス %s が見つかりません。</translation>
     </message>
     <message>
         <source>Did not get permission to erase the target device.</source>
-        <translation type="unfinished"></translation>
+        <translation>対象デバイスを消去する許可を得られませんでした。</translation>
     </message>
     <message>
         <source>The installer cannot be launched.</source>
-        <translation type="unfinished"></translation>
+        <translation>インストーラーを起動できません。</translation>
     </message>
     <message>
         <source>The installation did not succeed. Please see the Installer Log for more information.</source>
-        <translation type="unfinished"></translation>
+        <translation>インストールに成功しませんでした。詳しくは、インストーラーログをご覧ください。</translation>
     </message>
     <message>
         <source>Installation Complete</source>
-        <translation type="unfinished"></translation>
+        <translation>インストール完了</translation>
     </message>
     <message>
         <source>The installation succeeded.</source>
-        <translation type="unfinished"></translation>
+        <translation>インストールに成功しました。</translation>
     </message>
     <message>
         <source>FreeBSD has been installed on your computer, click &apos;Restart&apos; to begin using it.</source>
-        <translation type="unfinished"></translation>
+        <translation>FreeBSDがコンピュータにインストールされましたので、「再起動」をクリックして使用を開始します。</translation>
     </message>
     <message>
         <source>Restart</source>
-        <translation type="unfinished"></translation>
+        <translation>再起動</translation>
     </message>
     <message>
         <source>Could not restart the computer.</source>
-        <translation type="unfinished"></translation>
+        <translation>コンピュータを再起動できませんでした。</translation>
     </message>
     <message>
         <source>Error</source>
@@ -201,7 +201,7 @@
     </message>
     <message>
         <source>Quit</source>
-        <translation>やめておく</translation>
+        <translation>中止</translation>
     </message>
 </context>
 <context>
