fb9960735598e009f8ef727f661f31a5cd7c09be
probonopd <probonopd@users.noreply.github.com>
2023-02-22 22:31:56 +0100

    Add fullname [ci skip]

--- Preferences/Users.app/Resources/adduser.sh
+++ Preferences/Users.app/Resources/adduser.sh
@@ -9,15 +9,17 @@ if ! [ $(id -u) = 0 ]; then
 fi
 
 if [ -z "$1" ] ; then
-  echo "USAGE: ${0} username password"
+  echo "USAGE: ${0} username fullname password"
   exit 1
 fi
 
 username="$1"
-password="$2"
+fullname="$2"
+password="$3"
 
 echo "${username}:::::::::${password}" | adduser -f -
 pw usermod "${username}" -s /usr/local/bin/zsh
+pw usermod "${username}" -c "${fullname}"
 pw groupmod wheel -m "${username}"
 pw groupmod video -m "${username}"
 pw groupmod webcamd -m "${username}"
