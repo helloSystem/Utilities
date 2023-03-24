110b7fe2b091e58c903d1e10128a74d6bbbcd3a3
gnu-ewm <gnu.ewm@protonmail.com>
2023-03-24 15:29:14 +0000

    Translated using Weblate (Polish)
    Currently translated at 100.0% (20 of 20 strings)
    
    Translation: helloSystem/Hardware Probe
    Translate-URL: https://hosted.weblate.org/projects/hellosystem/hardware-probe/pl/

--- Utilities/Hardware Probe.app/Resources/i18n/pl.ts
+++ Utilities/Hardware Probe.app/Resources/i18n/pl.ts
@@ -9,7 +9,7 @@
     </message>
     <message>
         <source>Hardware Probe</source>
-        <translation type="unfinished"></translation>
+        <translation>Sonda sprzętowa</translation>
     </message>
     <message>
         <source>Privacy Information</source>
@@ -17,7 +17,7 @@
     </message>
     <message>
         <source>Uploading a Hardware Probe is subject to the following Pricacy Terms.</source>
-        <translation type="unfinished"></translation>
+        <translation>Wysyłanie sondy sprzętowej jest obiektem tej polityki prywatności.</translation>
     </message>
     <message>
         <source>Please see %s for more information.</source>
@@ -27,43 +27,45 @@
         <source>&lt;p&gt;This utility collects hardware details of your computer and can anonymously upload them to a public database.&lt;/p&gt;
         &lt;p&gt;This can help users and operating system developers to collaboratively debug hardware related issues, check for operating system compatibility and find drivers.&lt;/p&gt;
         &lt;p&gt;You will get a permanent probe URL to view and share collected information.&lt;/p&gt;&lt;br&gt;&lt;br&gt;&lt;br&gt;</source>
-        <translation type="unfinished"></translation>
+        <translation>&lt;p&gt;To narzędzie zbiera szczegóły dotyczące sprzętu komputera i może anonimowo przesłać je do publicznej bazy danych&lt;/p&gt;
+        &lt;p&gt;Może to pomóc użytkownikom i programistom systemu operacyjnego we wspólnym rozwiązywaniu problemów związanych ze sprzętem, sprawdzaniu zgodności systemu operacyjnego i szukaniu sterowników.&lt;/p&gt;
+         &lt;p&gt;Otrzymasz stały adres URL sondy, aby przeglądać i udostępniać zebrane informacje.&lt;/p&gt;&lt;br&gt;&lt;br&gt;&lt;br&gt;</translation>
     </message>
     <message>
         <source>Show Hardware Probe</source>
-        <translation type="unfinished"></translation>
+        <translation>Pokaż sondę sprzętową</translation>
     </message>
     <message>
         <source>Failed to open the hardware probe.</source>
-        <translation type="unfinished"></translation>
+        <translation>Nie udało się otworzyć sondy sprzętowej.</translation>
     </message>
     <message>
         <source>Failed to run the %s tool.</source>
-        <translation type="unfinished"></translation>
+        <translation>Nie udało się uruchomić %s.</translation>
     </message>
     <message>
         <source>Failed to set the owner to %x.</source>
-        <translation type="unfinished"></translation>
+        <translation>Nie udało się zmienić właściciela na %x.</translation>
     </message>
     <message>
         <source>Uploading Hardware Probe</source>
-        <translation type="unfinished"></translation>
+        <translation>Wysyłanie sondy sprzętowej</translation>
     </message>
     <message>
         <source>The Hardware Probe is being uploaded to the public database</source>
-        <translation type="unfinished"></translation>
+        <translation>Sonda sprzętowa została wysłana do publicznej bazy danych</translation>
     </message>
     <message>
         <source>You need an active internet connection in order to upload.</source>
-        <translation type="unfinished"></translation>
+        <translation>Potrzebujesz aktywnego połączenia z internetem, aby móc wysyłać.</translation>
     </message>
     <message>
         <source>Failed to upload using the %s tool.</source>
-        <translation type="unfinished"></translation>
+        <translation>Nie udało się wysłać urzywając %s.</translation>
     </message>
     <message>
         <source>Hardware Probe Uploaded</source>
-        <translation type="unfinished"></translation>
+        <translation>Wysłano sondę sprzętową</translation>
     </message>
     <message>
         <source>Quit</source>
@@ -71,7 +73,7 @@
     </message>
     <message>
         <source>Failed to open the uploaded hardware probe.</source>
-        <translation type="unfinished"></translation>
+        <translation>Nie udało się otworzyć wysłanej sondy sprzętowej.</translation>
     </message>
     <message>
         <source>Error</source>
@@ -79,7 +81,7 @@
     </message>
     <message>
         <source>Hardware Probe was not successful.</source>
-        <translation type="unfinished"></translation>
+        <translation>Sonda sprzętowa nie powiodła się.</translation>
     </message>
 </context>
 <context>
