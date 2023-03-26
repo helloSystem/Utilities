cf6e8f332068c22e581452188752a7dd301d4609
David Liang <2188741623@qq.com>
2023-03-26 06:16:43 +0000

    Translated using Weblate (Chinese (Simplified))
    Currently translated at 100.0% (29 of 29 strings)
    
    Translation: helloSystem/Create Live Media
    Translate-URL: https://hosted.weblate.org/projects/hellosystem/create-live-media/zh_Hans/

--- Utilities/Create Live Media.app/Resources/i18n/zh_Hans.ts
+++ Utilities/Create Live Media.app/Resources/i18n/zh_Hans.ts
@@ -29,98 +29,98 @@
     </message>
     <message>
         <source>Show Pre-release builds</source>
-        <translation type="unfinished"></translation>
+        <translation>显示预发行构建</translation>
     </message>
     <message>
         <source>Other</source>
-        <translation type="unfinished"></translation>
+        <translation>其它</translation>
     </message>
     <message>
         <source>URL of the ISO</source>
-        <translation type="unfinished"></translation>
+        <translation>ISO 的网址</translation>
     </message>
     <message>
         <source>Disk images (*.iso *.img);;All files (*.*)</source>
-        <translation type="unfinished"></translation>
+        <translation>磁盘镜像（*.iso *.img）;;所有文件（*.*）</translation>
     </message>
     <message>
         <source>This requires an active internet connection.</source>
-        <translation type="unfinished"></translation>
+        <translation>这需要一个活跃的互联网连接。</translation>
     </message>
     <message>
         <source>Select Destination Disk</source>
-        <translation type="unfinished"></translation>
+        <translation>选择目标磁盘</translation>
     </message>
     <message>
         <source>All data on the selected disk will be erased.</source>
-        <translation type="unfinished"></translation>
+        <translation>所选磁盘上的所有数据将被删除。</translation>
     </message>
     <message>
         <source>Could not determine the required disk space.</source>
-        <translation type="unfinished"></translation>
+        <translation>无法确定所需的磁盘空间。</translation>
     </message>
     <message>
         <source>Disk space required: %s MiB</source>
-        <translation type="unfinished"></translation>
+        <translation>需要: %s MiB 的磁盘空间</translation>
     </message>
     <message>
         <source>Warning</source>
-        <translation type="unfinished"></translation>
+        <translation>警告</translation>
     </message>
     <message>
         <source>This will erase all contents of this disk and install the live system on it. Continue?</source>
-        <translation type="unfinished"></translation>
+        <translation>这将清除该磁盘的所有内容，并在其上安装新的系统。继续吗？</translation>
     </message>
     <message>
         <source>Downloading and writing Live medium</source>
-        <translation type="unfinished"></translation>
+        <translation>下载和编写实况媒体</translation>
     </message>
     <message>
         <source>The Live image is being downloaded and written to the medium.</source>
-        <translation type="unfinished"></translation>
+        <translation>实时镜像正在被下载并写入介质。</translation>
     </message>
     <message>
         <source>Could not unmount parititons.</source>
-        <translation type="unfinished"></translation>
+        <translation>无法卸载分区。</translation>
     </message>
     <message>
         <source>An error occurred while trying to write the image. Is the download URL accessible? Were all partitions unmounted? Do you have write permissions there?</source>
-        <translation type="unfinished"></translation>
+        <translation>在尝试写入镜像时发生了错误。下载的URL是否可以访问？所有的分区都被卸载了吗？分区有写的权限吗？</translation>
     </message>
     <message>
         <source>Live Medium Complete</source>
-        <translation type="unfinished"></translation>
+        <translation>镜像写入完成</translation>
     </message>
     <message>
         <source>The Live image has been written to the device.</source>
-        <translation type="unfinished"></translation>
+        <translation>实时镜像已被写入设备中。</translation>
     </message>
     <message>
         <source>You can now start your computer from the Live medium.</source>
-        <translation type="unfinished"></translation>
+        <translation>现在你可以从介质中启动你的计算机。</translation>
     </message>
     <message>
         <source>Quit</source>
-        <translation type="unfinished"></translation>
+        <translation>放弃</translation>
     </message>
     <message>
         <source>Device was unplugged, exiting</source>
-        <translation type="unfinished"></translation>
+        <translation>设备已拔出，正在退出</translation>
     </message>
     <message>
         <source>Error</source>
-        <translation type="unfinished"></translation>
+        <translation>错误</translation>
     </message>
     <message>
         <source>The installation could not be performed.</source>
-        <translation type="unfinished"></translation>
+        <translation>无法进行安装。</translation>
     </message>
 </context>
 <context>
     <name>tstr</name>
     <message>
         <source>Hello World</source>
-        <translation type="unfinished"></translation>
+        <translation>你好 世界</translation>
     </message>
 </context>
 </TS>
