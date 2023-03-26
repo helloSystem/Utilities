492e50deef7c1ebcdb120b481544932070c5ecc8
David Liang <2188741623@qq.com>
2023-03-26 07:35:24 +0000

    Translated using Weblate (Chinese (Simplified))
    Currently translated at 100.0% (20 of 20 strings)
    
    Translation: helloSystem/Hardware Probe
    Translate-URL: https://hosted.weblate.org/projects/hellosystem/hardware-probe/zh_Hans/

--- Utilities/Hardware Probe.app/Resources/i18n/zh_Hans.ts
+++ Utilities/Hardware Probe.app/Resources/i18n/zh_Hans.ts
@@ -17,76 +17,78 @@
     </message>
     <message>
         <source>Uploading a Hardware Probe is subject to the following Pricacy Terms.</source>
-        <translation type="unfinished"></translation>
+        <translation>上传硬件探测受以下原则条款的约束。</translation>
     </message>
     <message>
         <source>Please see %s for more information.</source>
-        <translation type="unfinished"></translation>
+        <translation>更多信息请见 %s 。</translation>
     </message>
     <message>
         <source>&lt;p&gt;This utility collects hardware details of your computer and can anonymously upload them to a public database.&lt;/p&gt;
         &lt;p&gt;This can help users and operating system developers to collaboratively debug hardware related issues, check for operating system compatibility and find drivers.&lt;/p&gt;
         &lt;p&gt;You will get a permanent probe URL to view and share collected information.&lt;/p&gt;&lt;br&gt;&lt;br&gt;&lt;br&gt;</source>
-        <translation type="unfinished"></translation>
+        <translation>&lt;p&gt;这个工具收集了你的计算机的硬件细节，并可以匿名将它们上传到一个公共数据库。
+        &lt;p&gt;这可以帮助用户和操作系统开发人员协同调试与硬件有关的问题，检查操作系统的兼容性并查找驱动程序。
+        &lt;p&gt;您将得到一个永久的探测URL，以查看和分享收集的信息。&lt;/p&gt;&lt;br&gt;&lt;br&gt;&lt;br&gt;&lt;br&gt;</translation>
     </message>
     <message>
         <source>Show Hardware Probe</source>
-        <translation type="unfinished"></translation>
+        <translation>显示硬件信息</translation>
     </message>
     <message>
         <source>Failed to open the hardware probe.</source>
-        <translation type="unfinished"></translation>
+        <translation>打开硬件信息失败。</translation>
     </message>
     <message>
         <source>Failed to run the %s tool.</source>
-        <translation type="unfinished"></translation>
+        <translation>运行 %s 工具失败。</translation>
     </message>
     <message>
         <source>Failed to set the owner to %x.</source>
-        <translation type="unfinished"></translation>
+        <translation>未能将所有者设置为 %x 。</translation>
     </message>
     <message>
         <source>Uploading Hardware Probe</source>
-        <translation type="unfinished"></translation>
+        <translation>上传硬件信息</translation>
     </message>
     <message>
         <source>The Hardware Probe is being uploaded to the public database</source>
-        <translation type="unfinished"></translation>
+        <translation>硬件信息正在被上传到公共数据库中</translation>
     </message>
     <message>
         <source>You need an active internet connection in order to upload.</source>
-        <translation type="unfinished"></translation>
+        <translation>你需要一个活跃的互联网连接，以便上传。</translation>
     </message>
     <message>
         <source>Failed to upload using the %s tool.</source>
-        <translation type="unfinished"></translation>
+        <translation>使用 %s 工具上传失败。</translation>
     </message>
     <message>
         <source>Hardware Probe Uploaded</source>
-        <translation type="unfinished"></translation>
+        <translation>上传的硬件信息</translation>
     </message>
     <message>
         <source>Quit</source>
-        <translation type="unfinished"></translation>
+        <translation>放弃</translation>
     </message>
     <message>
         <source>Failed to open the uploaded hardware probe.</source>
-        <translation type="unfinished"></translation>
+        <translation>未能打开上传的硬件信息。</translation>
     </message>
     <message>
         <source>Error</source>
-        <translation type="unfinished"></translation>
+        <translation>错误</translation>
     </message>
     <message>
         <source>Hardware Probe was not successful.</source>
-        <translation type="unfinished"></translation>
+        <translation>硬件探测失败。</translation>
     </message>
 </context>
 <context>
     <name>tstr</name>
     <message>
         <source>Hello World</source>
-        <translation type="unfinished"></translation>
+        <translation>你好 世界</translation>
     </message>
 </context>
 </TS>
