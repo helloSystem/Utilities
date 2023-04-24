272029b661e43fef615f206af2f5be0df34d2c75
Devid Zanon <d.zanon93@gmail.com>
2023-04-24 19:35:08 +0000

    Translated using Weblate (Italian)
    Currently translated at 100.0% (20 of 20 strings)
    
    Translation: helloSystem/Hardware Probe
    Translate-URL: https://hosted.weblate.org/projects/hellosystem/hardware-probe/it/

--- Utilities/Hardware Probe.app/Resources/i18n/it.ts
+++ Utilities/Hardware Probe.app/Resources/i18n/it.ts
@@ -9,61 +9,63 @@
     </message>
     <message>
         <source>Hardware Probe</source>
-        <translation type="unfinished"></translation>
+        <translation>Sonda l&apos;hardware</translation>
     </message>
     <message>
         <source>Privacy Information</source>
-        <translation type="unfinished"></translation>
+        <translation>Informazioni sulla Privacy</translation>
     </message>
     <message>
         <source>Uploading a Hardware Probe is subject to the following Pricacy Terms.</source>
-        <translation type="unfinished"></translation>
+        <translation>Il caricamento del sondaggio Hardware è soggetto ai seguenti termini sulla privacy.</translation>
     </message>
     <message>
         <source>Please see %s for more information.</source>
-        <translation type="unfinished"></translation>
+        <translation>Guarda %s per maggiori informazioni.</translation>
     </message>
     <message>
         <source>&lt;p&gt;This utility collects hardware details of your computer and can anonymously upload them to a public database.&lt;/p&gt;
         &lt;p&gt;This can help users and operating system developers to collaboratively debug hardware related issues, check for operating system compatibility and find drivers.&lt;/p&gt;
         &lt;p&gt;You will get a permanent probe URL to view and share collected information.&lt;/p&gt;&lt;br&gt;&lt;br&gt;&lt;br&gt;</source>
-        <translation type="unfinished"></translation>
+        <translation>&lt;p&gt;Questo programma raccoglie i dettagli del tuo hardware e può caricarli anonimamente in un database pubblico.&lt;/p&gt;
+        &lt;p&gt;Permette agli utenti e agli sviluppatori del SO di effettuare il debug di problematiche relative all&apos;hardware, controllare la compatibilità del SO e trovare i driver.&lt;/p&gt;
+        &lt;p&gt;Otterrai un indirizzo URL permanente per visualizzare e condividere le informazioni raccolte.&lt;/p&gt;&lt;br&gt;&lt;br&gt;&lt;br&gt;</translation>
     </message>
     <message>
         <source>Show Hardware Probe</source>
-        <translation type="unfinished"></translation>
+        <translation>Mostra sondaggio hardware</translation>
     </message>
     <message>
         <source>Failed to open the hardware probe.</source>
-        <translation type="unfinished"></translation>
+        <translation>Apertura del sondaggio hardware non riuscita.</translation>
     </message>
     <message>
         <source>Failed to run the %s tool.</source>
-        <translation type="unfinished"></translation>
+        <translation>Impossibile eseguire lo strumento %s.</translation>
     </message>
     <message>
         <source>Failed to set the owner to %x.</source>
-        <translation type="unfinished"></translation>
+        <translation>Impostazione del proprietario di %x non riuscita.</translation>
     </message>
     <message>
         <source>Uploading Hardware Probe</source>
-        <translation type="unfinished"></translation>
+        <translation>Caricamento sondaggio hardware</translation>
     </message>
     <message>
         <source>The Hardware Probe is being uploaded to the public database</source>
-        <translation type="unfinished"></translation>
+        <translation>Il sondaggio hardware si sta caricando nel database pubblico</translation>
     </message>
     <message>
         <source>You need an active internet connection in order to upload.</source>
-        <translation type="unfinished"></translation>
+        <translation>E&apos; necessaria una connessione internet per effettuare il caricamento.</translation>
     </message>
     <message>
         <source>Failed to upload using the %s tool.</source>
-        <translation type="unfinished"></translation>
+        <translation>Caricamento con lo strumento %s non riuscito.</translation>
     </message>
     <message>
         <source>Hardware Probe Uploaded</source>
-        <translation type="unfinished"></translation>
+        <translation>Sondaggio hardware caricato</translation>
     </message>
     <message>
         <source>Quit</source>
@@ -71,7 +73,7 @@
     </message>
     <message>
         <source>Failed to open the uploaded hardware probe.</source>
-        <translation type="unfinished"></translation>
+        <translation>Apertura nel sondaggio hardware caricato non riuscita.</translation>
     </message>
     <message>
         <source>Error</source>
@@ -79,7 +81,7 @@
     </message>
     <message>
         <source>Hardware Probe was not successful.</source>
-        <translation type="unfinished"></translation>
+        <translation>Sondaggio hardware non riuscito.</translation>
     </message>
 </context>
 <context>
