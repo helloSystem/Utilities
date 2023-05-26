7177b20d88958046b3769d5410958862e75c56ef
Tuuux <tuxa@rtnp.org>
2023-05-26 12:23:12 +0200

    Plug menu signals

--- Under Construction/Activity Monitor.app/Resources/main.py
+++ Under Construction/Activity Monitor.app/Resources/main.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 import sys
-from PyQt5.QtCore import Qt
+from PyQt5.QtCore import Qt, QTimer
 from PyQt5.QtWidgets import (
     QApplication,
     QMainWindow,
@@ -12,6 +12,7 @@ from PyQt5.QtWidgets import (
     QWidgetAction,
     QLineEdit,
     QComboBox,
+
 )
 from ui import Ui_MainWindow
 
@@ -39,6 +40,10 @@ class Window(QMainWindow, Ui_MainWindow):
 
         self.setupUi(self)
         self.setupCustomUi()
+
+        self.timer = QTimer()
+        self._timer_change_for_5_secs()
+
         self.connectSignalsSlots()
 
     def setupCustomUi(self):
@@ -47,9 +52,9 @@ class Window(QMainWindow, Ui_MainWindow):
 
     def setupCustomUiGroups(self):
         menu_frequency_group = QActionGroup(self)
-        menu_frequency_group.addAction(self.ActionFrequencyTo1Sec)
-        menu_frequency_group.addAction(self.ActionFrequencyTo3Secs)
-        menu_frequency_group.addAction(self.ActionFrequencyTo5Secs)
+        menu_frequency_group.addAction(self.ActionUpdateFrequencyTo1Sec)
+        menu_frequency_group.addAction(self.ActionUpdateFrequencyTo3Secs)
+        menu_frequency_group.addAction(self.ActionUpdateFrequencyTo5Secs)
 
         menu_filter_by_group = QActionGroup(self)
         menu_filter_by_group.addAction(self.ActionMenuViewAllProcesses)
@@ -102,8 +107,108 @@ class Window(QMainWindow, Ui_MainWindow):
         self.toolBar.addAction(self.search_process_action)
 
     def connectSignalsSlots(self):
+        self.timer.timeout.connect(self.refresh)
+
+        self.ActionUpdateFrequencyTo5Secs.triggered.connect(self._timer_change_for_5_secs)
+        self.ActionUpdateFrequencyTo3Secs.triggered.connect(self._timer_change_for_3_secs)
+        self.ActionUpdateFrequencyTo1Sec.triggered.connect(self._timer_change_for_1_sec)
+
+        # self.searchLineEdit.textChanged.connect(self.process_monitor.refresh)
+        self.filterComboBox.currentIndexChanged.connect(self._filter_by_changed)
+
+        self.ActionMenuViewAllProcesses.triggered.connect(self._filter_by_all_processes)
+        self.ActionMenuViewAllProcessesHierarchically.triggered.connect(self._filter_by_all_process_hierarchically)
+        self.ActionMenuViewMyProcesses.triggered.connect(self._filter_by_my_processes)
+        self.ActionMenuViewSystemProcesses.triggered.connect(self._filter_by_system_processes)
+        self.ActionMenuViewOtherUserProcesses.triggered.connect(self._filter_by_other_user_processes)
+        self.ActionMenuViewActiveProcesses.triggered.connect(self._filter_by_active_processes)
+        self.ActionMenuViewInactiveProcesses.triggered.connect(self._filter_by_inactive_processes)
+        self.ActionMenuViewWindowedProcesses.triggered.connect(self._filter_by_windowed_processes)
+        self.ActionMenuViewSelectedProcesses.triggered.connect(self._filter_by_selected_processes)
+        self.ActionMenuViewApplicationInLast12Hours.triggered.connect(self._filter_by_application_in_last_12_hours)
+
+    def refresh(self):
         pass
 
+    def _timer_change_for_1_sec(self):
+        self.timer.start(1000)
+        if self.tab_disk_activity:
+            self.tab_disk_activity.timer_value = 1
+
+    def _timer_change_for_3_secs(self):
+        self.timer.start(3000)
+        if self.tab_disk_activity:
+            self.tab_disk_activity.timer_value = 3
+
+    def _timer_change_for_5_secs(self):
+        self.timer.start(5000)
+        if self.tab_disk_activity:
+            self.tab_disk_activity.timer_value = 5
+
+    def _filter_by_changed(self):
+        #             0: 'All Processes',
+        #             1: 'All Processes, Hierarchically',
+        #             2: 'My Processes',
+        #             3: 'System Processes',
+        #             4: 'Other User Processes',
+        #             5: 'Active Processes',
+        #             6: 'Inactive Processes',
+        #             7: 'Windowed Processes',
+        #             8: 'Selected Processes',
+        #             9: 'Application in last 12 hours',
+        if hasattr(self.filterComboBox, "currentIndex"):
+            if self.filterComboBox.currentIndex() == 0:
+                self.ActionMenuViewAllProcesses.setChecked(True)
+            elif self.filterComboBox.currentIndex() == 1:
+                self.ActionMenuViewAllProcessesHierarchically.setChecked(True)
+            elif self.filterComboBox.currentIndex() == 2:
+                self.ActionMenuViewMyProcesses.setChecked(True)
+            elif self.filterComboBox.currentIndex() == 3:
+                self.ActionMenuViewSystemProcesses.setChecked(True)
+            elif self.filterComboBox.currentIndex() == 4:
+                self.ActionMenuViewOtherUserProcesses.setChecked(True)
+            elif self.filterComboBox.currentIndex() == 5:
+                self.ActionMenuViewActiveProcesses.setChecked(True)
+            elif self.filterComboBox.currentIndex() == 6:
+                self.ActionMenuViewInactiveProcesses.setChecked(True)
+            elif self.filterComboBox.currentIndex() == 7:
+                self.ActionMenuViewWindowedProcesses.setChecked(True)
+            elif self.filterComboBox.currentIndex() == 8:
+                self.ActionMenuViewSelectedProcesses.setChecked(True)
+            elif self.filterComboBox.currentIndex() == 9:
+                self.ActionMenuViewApplicationInLast12Hours.setChecked(True)
+
+        # self.process_monitor.refresh()
+
+    def _filter_by_all_processes(self):
+        self.filterComboBox.setCurrentIndex(0)
+
+    def _filter_by_all_process_hierarchically(self):
+        self.filterComboBox.setCurrentIndex(1)
+
+    def _filter_by_my_processes(self):
+        self.filterComboBox.setCurrentIndex(2)
+
+    def _filter_by_system_processes(self):
+        self.filterComboBox.setCurrentIndex(3)
+
+    def _filter_by_other_user_processes(self):
+        self.filterComboBox.setCurrentIndex(4)
+
+    def _filter_by_active_processes(self):
+        self.filterComboBox.setCurrentIndex(5)
+
+    def _filter_by_inactive_processes(self):
+        self.filterComboBox.setCurrentIndex(6)
+
+    def _filter_by_windowed_processes(self):
+        self.filterComboBox.setCurrentIndex(7)
+
+    def _filter_by_selected_processes(self):
+        self.filterComboBox.setCurrentIndex(8)
+
+    def _filter_by_application_in_last_12_hours(self):
+        self.filterComboBox.setCurrentIndex(9)
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
