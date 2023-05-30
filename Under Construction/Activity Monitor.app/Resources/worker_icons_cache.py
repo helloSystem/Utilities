bd0fc34c9a4caddee6399d5159cce6642a5cb0ce
Tuuux <tuxa@rtnp.org>
2023-05-30 13:14:30 +0200

    Make tests

--- Under Construction/Activity Monitor.app/Resources/worker_icons_cache.py
+++ Under Construction/Activity Monitor.app/Resources/worker_icons_cache.py
@@ -21,46 +21,67 @@ class IconsCacheWorker(QObject):
 
     def refresh(self):
         for p in psutil.process_iter():
-            if p.name() not in self.cache:
-                try:
-                    if "LAUNCHED_BUNDLE" in p.environ():
-                        # Without the path, only the bundle name, without the last suffix
-                        bundle_path = p.environ()["LAUNCHED_BUNDLE"]
-                        # bundle_name = bundle_path.split("/")[-1].split(".")[0]
-                        bundle_name = os.path.basename(bundle_path).rsplit(".", 1)[0]
+            icon = None
+            application_name = None
+            try:
+                environ = p.environ()
+            except psutil.AccessDenied:
+                environ = None
+            if environ and "LAUNCHED_BUNDLE" in environ:
+                application_name = os.path.basename(environ["LAUNCHED_BUNDLE"]).rsplit(".", 1)[0]
+                for icon_suffix in [".png", ".jpg", ".xpg", ".svg", ".xpm"]:
+                    icon_path = os.path.join(
+                        environ["LAUNCHED_BUNDLE"],
+                        "Resources",
+                        f"{application_name}{icon_suffix.lower()}"
+                    )
+                    print(f"Try: {icon_path}")
+                    if os.path.exists(icon_path):
+                        icon = QIcon(icon_path)
+                        break
+            else:
+                application_name = p.name()
 
-                        # Get the application bundle icon
-                        # AppDir
-                        os.path.exists(os.path.join(bundle_path, "DirIcon"))
-                        if os.path.exists(os.path.join(bundle_path, "DirIcon")):
-                            # self.updated_icons_cache.emit({p.name(): QIcon(os.path.join(bundle_path, "DirIcon"))})
-                            self.updated_icons_cache.emit(
-                                {f"{bundle_name}": QIcon(os.path.join(bundle_path, "DirIcon"))}
-                            )
-                        else:
-                            # .app
-                            for icon_suffix in [".png", ".jpg", ".xpg", ".svg", ".xpm"]:
-                                icon_path = os.path.join(bundle_path, "Resources", f"{bundle_name}{icon_suffix.lower()}")
-                                print(f"Try: {icon_path}")
-                                if os.path.exists(icon_path):
-                                    print(f"Found: {icon_path}")
-                                    self.updated_icons_cache.emit(
-                                        {f"{bundle_name}": QIcon(icon_path)}
-                                    )
-                                    break
+            if icon is None:
+                icon = QIcon.fromTheme(p.name().lower())
+
+            if application_name not in self.cache:
+                self.updated_icons_cache.emit({application_name: icon})
+
+                # Get the application bundle icon
+                # AppDir
+                # os.path.exists(os.path.join(bundle_path, "DirIcon"))
+                # if os.path.exists(os.path.join(bundle_path, "DirIcon")):
+                #     # self.updated_icons_cache.emit({p.name(): QIcon(os.path.join(bundle_path, "DirIcon"))})
+                #     icon = QIcon(os.path.join(bundle_path, "DirIcon"))
+                #     self.updated_icons_cache.emit(
+                #         {f"{bundle_name}": QIcon(os.path.join(bundle_path, "DirIcon"))}
+                #     )
+                # else:
+                #     # .app
+                #     for icon_suffix in [".png", ".jpg", ".xpg", ".svg", ".xpm"]:
+                #         icon_path = os.path.join(bundle_path, "Resources", f"{bundle_name}{icon_suffix.lower()}")
+                #         print(f"Try: {icon_path}")
+                #         if os.path.exists(icon_path):
+                #             print(f"Found: {icon_path}")
+                #             self.updated_icons_cache.emit(
+                #                 {f"{bundle_name}": QIcon(icon_path)}
+                #             )
+                #             break
                         # XDG thumbnails for AppImages; TODO: Test this
-                        if bundle_path.endswith(".AppImage"):
-                            # xdg_thumbnail_path = os.path.join(xdg.BaseDirectory.xdg_cache_home, "thumbnails", "normal")
-                            xdg_thumbnail_path = os.path.expanduser("~/.cache/thumbnails/normal")
-                            # print(xdg_thumbnail_path)
-                            xdg_thumbnail_path = os.path.join(
-                                xdg_thumbnail_path,
-                                hashlib.md5(bundle_path.encode("utf-8")).hexdigest() + ".png")
-                            if os.path.exists(xdg_thumbnail_path):
-                                icon = QIcon(xdg_thumbnail_path)
-                                self.updated_icons_cache.emit({p.name(): icon})
-                    else:
-                        self.updated_icons_cache.emit({f"{p.name()}": QIcon.fromTheme(p.name().lower())})
-                except psutil.AccessDenied:
-                    self.updated_icons_cache.emit({f"{p.name()}": QIcon.fromTheme(p.name().lower())})
+                        # if bundle_path.endswith(".AppImage"):
+                        #     # xdg_thumbnail_path = os.path.join(xdg.BaseDirectory.xdg_cache_home, "thumbnails", "normal")
+                        #     xdg_thumbnail_path = os.path.expanduser("~/.cache/thumbnails/normal")
+                        #     # print(xdg_thumbnail_path)
+                        #     xdg_thumbnail_path = os.path.join(
+                        #         xdg_thumbnail_path,
+                        #         hashlib.md5(bundle_path.encode("utf-8")).hexdigest() + ".png")
+                        #     if os.path.exists(xdg_thumbnail_path):
+                        #         icon = QIcon(xdg_thumbnail_path)
+                        #         self.updated_icons_cache.emit({p.name(): icon})
+
+                    # else:
+                    #     self.updated_icons_cache.emit({f"{p.name()}": QIcon.fromTheme(p.name().lower())})
+                # except psutil.AccessDenied:
+                #     self.updated_icons_cache.emit({f"{p.name()}": QIcon.fromTheme(p.name().lower())})
         self.finished.emit()
