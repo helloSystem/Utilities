88c8ef8aed7cc9cb1fee8164db7a20791fef753c
Tuuux <tuxa@rtnp.org>
2023-06-13 11:28:37 +0200

    The end is near

--- Under Construction/Activity Monitor.app/Resources/main_window_ui.py
+++ Under Construction/Activity Monitor.app/Resources/main_window_ui.py
@@ -14,7 +14,7 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
-        MainWindow.resize(727, 669)
+        MainWindow.resize(633, 536)
         MainWindow.setFocusPolicy(QtCore.Qt.StrongFocus)
         icon = QtGui.QIcon()
         icon.addPixmap(QtGui.QPixmap(":/Processes.png"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
@@ -824,7 +824,7 @@ class Ui_MainWindow(object):
         self.verticalLayout_4.addWidget(self.central_widget_tabs)
         MainWindow.setCentralWidget(self.central_widget)
         self.menubar = QtWidgets.QMenuBar(MainWindow)
-        self.menubar.setGeometry(QtCore.QRect(0, 0, 727, 24))
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 633, 24))
         self.menubar.setObjectName("menubar")
         self.menuFile = QtWidgets.QMenu(self.menubar)
         self.menuFile.setObjectName("menuFile")
