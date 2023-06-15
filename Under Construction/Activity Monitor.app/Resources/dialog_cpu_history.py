1533eee841ccc2af7dec1fe53db66f822142c586
Tuuux <tuxa@rtnp.org>
2023-06-15 08:27:04 +0200

    Remove not used signals, and add comments about why that is code like that

--- Under Construction/Activity Monitor.app/Resources/dialog_cpu_history.py
+++ Under Construction/Activity Monitor.app/Resources/dialog_cpu_history.py
@@ -1,24 +1,20 @@
-import os
-import signal
-
-from PyQt5.QtWidgets import QDialog, QWidget
-from PyQt5.QtCore import pyqtSignal, Qt
+from PyQt5.QtWidgets import QWidget
 from dialog_cpu_history_ui import Ui_CPUHistory
 
 
 class CPUHistory(QWidget, Ui_CPUHistory):
-    process_signal_quit = pyqtSignal()
-    process_signal_kill = pyqtSignal()
 
     def __init__(self, parent=None, process=None):
         super(CPUHistory, self).__init__(parent)
         self.setupUi(self)
         self.process = process
 
-        # when you want to destroy the dialog set this to True
+        # When you want to destroy the dialog set this to True
         self.have_to_close = False
 
     def closeEvent(self, evnt):
+        # Yes that widget is call as a window, them MUST be close with the main app
+        # Else ---> The widget is hide then continue to store CPU data
         if self.have_to_close:
             super(CPUHistory, self).closeEvent(evnt)
         else:
