1773d0fe603aa44c2fa379afab1ccdbde0b2fd98
louies0623 <louies0623@gmail.com>
2023-06-21 15:19:25 +0000

    Translated using Weblate (Chinese (Traditional))
    Currently translated at 100.0% (20 of 20 strings)
    
    Translation: helloSystem/Hardware Probe
    Translate-URL: https://hosted.weblate.org/projects/hellosystem/hardware-probe/zh_Hant/

--- Utilities/Hardware Probe.app/Resources/i18n/zh_Hant.ts
+++ Utilities/Hardware Probe.app/Resources/i18n/zh_Hant.ts
@@ -9,35 +9,35 @@
     </message>
     <message>
         <source>Hardware Probe</source>
-        <translation>硬體偵測</translation>
+        <translation>硬體探測器</translation>
     </message>
     <message>
         <source>Privacy Information</source>
-        <translation>內容隱私</translation>
+        <translation>隱私資訊</translation>
     </message>
     <message>
         <source>Uploading a Hardware Probe is subject to the following Pricacy Terms.</source>
-        <translation>上傳硬體偵測受以下隱私協議的約束。</translation>
+        <translation>上傳的硬體探測器受以下的隱私條款所約束。</translation>
     </message>
     <message>
         <source>Please see %s for more information.</source>
-        <translation>有關詳細內容，請查看 %s。</translation>
+        <translation>有關詳細資訊，請參閱 %s。</translation>
     </message>
     <message>
         <source>&lt;p&gt;This utility collects hardware details of your computer and can anonymously upload them to a public database.&lt;/p&gt;
         &lt;p&gt;This can help users and operating system developers to collaboratively debug hardware related issues, check for operating system compatibility and find drivers.&lt;/p&gt;
         &lt;p&gt;You will get a permanent probe URL to view and share collected information.&lt;/p&gt;&lt;br&gt;&lt;br&gt;&lt;br&gt;</source>
-        <translation>&lt;p&gt;此應用程式會收集您電腦硬體的詳細內容，並可匿名將它們上傳至公共資料庫。&lt;/p&gt;
+        <translation>&lt;p&gt;此應用程式會收集您電腦硬體的詳細資訊，並可匿名將它們上傳至公共資料庫。&lt;/p&gt;
         &lt;p&gt;這可以協助使用者和作業系統開發者合作偵測硬體的相關問題，檢查作業系統的兼容性並找到該驅動程式。&lt;/p&gt;
         &lt;p&gt;您將獲得一個永久偵測 URL 網址可以查看以及共用收集的內容。&lt;/p&gt;&lt;br&gt;&lt;br&gt;&lt;br&gt;</translation>
     </message>
     <message>
         <source>Show Hardware Probe</source>
-        <translation>顯示硬體偵測</translation>
+        <translation>顯示硬體探測器</translation>
     </message>
     <message>
         <source>Failed to open the hardware probe.</source>
-        <translation>無法開啟硬體偵測。</translation>
+        <translation>無法開啟硬體探測器。</translation>
     </message>
     <message>
         <source>Failed to run the %s tool.</source>
@@ -49,15 +49,15 @@
     </message>
     <message>
         <source>Uploading Hardware Probe</source>
-        <translation>正在上傳硬體偵測</translation>
+        <translation>正在上傳硬體探測器</translation>
     </message>
     <message>
         <source>The Hardware Probe is being uploaded to the public database</source>
-        <translation>硬體偵測正在上傳至公共資料庫</translation>
+        <translation>硬體探測器正在上傳至公共資料庫</translation>
     </message>
     <message>
         <source>You need an active internet connection in order to upload.</source>
-        <translation>您需要連接到可用的網際網路才能上傳。</translation>
+        <translation>您需要連接到有效的網際網路才能上傳。</translation>
     </message>
     <message>
         <source>Failed to upload using the %s tool.</source>
@@ -65,7 +65,7 @@
     </message>
     <message>
         <source>Hardware Probe Uploaded</source>
-        <translation>硬體偵測已上傳</translation>
+        <translation>硬體探測器已上傳</translation>
     </message>
     <message>
         <source>Quit</source>
@@ -73,7 +73,7 @@
     </message>
     <message>
         <source>Failed to open the uploaded hardware probe.</source>
-        <translation>無法開啟已上傳的硬體偵測。</translation>
+        <translation>無法開啟已上傳的硬體探測器。</translation>
     </message>
     <message>
         <source>Error</source>
@@ -81,7 +81,7 @@
     </message>
     <message>
         <source>Hardware Probe was not successful.</source>
-        <translation>硬體偵測並沒有成功。</translation>
+        <translation>硬體探測器並沒有成功。</translation>
     </message>
 </context>
 <context>
