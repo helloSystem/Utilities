9735083ca36ab7def72359a6bd21af459fd90e2d
Tuuux <tuxa@rtnp.org>
2023-06-21 20:15:10 +0200

    Network Activity about dialog remove version, and fixe typo.

--- Under Construction/Network Utility.app/Resources/dialog_about_ui.py
+++ Under Construction/Network Utility.app/Resources/dialog_about_ui.py
@@ -14,7 +14,7 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 class Ui_AboutDialog(object):
     def setupUi(self, AboutDialog):
         AboutDialog.setObjectName("AboutDialog")
-        AboutDialog.resize(338, 393)
+        AboutDialog.resize(344, 370)
         sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.MinimumExpanding, QtWidgets.QSizePolicy.MinimumExpanding)
         sizePolicy.setHorizontalStretch(0)
         sizePolicy.setVerticalStretch(0)
@@ -56,18 +56,6 @@ class Ui_AboutDialog(object):
         self.about_app_name.setWordWrap(False)
         self.about_app_name.setObjectName("about_app_name")
         self.verticalLayout_2.addWidget(self.about_app_name)
-        self.about_version = QtWidgets.QLabel(AboutDialog)
-        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Preferred, QtWidgets.QSizePolicy.Fixed)
-        sizePolicy.setHorizontalStretch(0)
-        sizePolicy.setVerticalStretch(0)
-        sizePolicy.setHeightForWidth(self.about_version.sizePolicy().hasHeightForWidth())
-        self.about_version.setSizePolicy(sizePolicy)
-        font = QtGui.QFont()
-        font.setPointSize(10)
-        self.about_version.setFont(font)
-        self.about_version.setWordWrap(True)
-        self.about_version.setObjectName("about_version")
-        self.verticalLayout_2.addWidget(self.about_version)
         spacerItem = QtWidgets.QSpacerItem(20, 0, QtWidgets.QSizePolicy.Minimum, QtWidgets.QSizePolicy.Expanding)
         self.verticalLayout_2.addItem(spacerItem)
         self.about_text = QtWidgets.QLabel(AboutDialog)
@@ -103,6 +91,5 @@ class Ui_AboutDialog(object):
     def retranslateUi(self, AboutDialog):
         _translate = QtCore.QCoreApplication.translate
         self.about_app_name.setText(_translate("AboutDialog", "<html><head/><body><p align=\"center\">Network Utility</p></body></html>"))
-        self.about_version.setText(_translate("AboutDialog", "<html><head/><body><p align=\"center\"><span style=\" font-size:14pt; color:#3d3846; vertical-align:super;\">Version 0.2rc1 </span></p></body></html>"))
         self.about_text.setText(_translate("AboutDialog", "<html><head/><body><p align=\"center\">This project is open souce, contributions are welcomed.<br/></p><p align=\"center\">Visit <a href=\"https://github.com/helloSystem/Utilities/\"><span style=\" text-decoration: underline; color:#0000ff;\">https://github.com/helloSystem/Utilities/</span></a> for more information or to report bug and/or suggest a new feature. </p></body></html>"))
-        self.about_footer.setText(_translate("AboutDialog", "<html><head/><body><p align=\"center\"><span style=\" font-size:14pt; vertical-align:sub;\">Make with love by: Jérôme ORNECH alias Hierosme<br/>Copyright 2023-2024 helloSystem Team. All right reserved. </span></p></body></html>"))
+        self.about_footer.setText(_translate("AboutDialog", "<html><head/><body><p align=\"center\"><span style=\" font-size:14pt; vertical-align:sub;\">Make with love by: Jérôme ORNECH alias Hierosme<br/>Copyright 2023-2024 helloSystem Team. All rights reserved. </span></p></body></html>"))
